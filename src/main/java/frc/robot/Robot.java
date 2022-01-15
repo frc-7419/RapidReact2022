105f4cd0d9e2013d5cd3b07886adcf176ba3389a
Karan Saini <karansaini117@gmail.com>
2022-01-15 15:48:59 -0800

    changed deprecated PIDController class to proper one.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
--- src/main/java/frc/robot/subsystems/limelight/TurnToTargetClosedLoop.java
+++ src/main/java/frc/robot/subsystems/limelight/TurnToTargetClosedLoop.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.limelight;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.LimelightConstants;
