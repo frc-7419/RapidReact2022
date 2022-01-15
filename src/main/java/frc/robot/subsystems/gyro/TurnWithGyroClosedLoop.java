47ee5b35c8351ae3a0c18eea856afa52c3144f60
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-01-15 15:49:39 -0800

    Changed deprecated PIDController to the new one.

--- src/main/java/frc/robot/subsystems/gyro/TurnWithGyroClosedLoop.java
+++ src/main/java/frc/robot/subsystems/gyro/TurnWithGyroClosedLoop.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems.gyro;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
