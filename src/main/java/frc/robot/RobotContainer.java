d3ef09fcae4d7fb9d57cac53f22c2f8b9b3f2fa3
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-01-15 15:27:00 -0800

    Added previous files from AdvPro2021 for limit-switch-with-motor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,69 +7,38 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.subsystems.limelight.TurnOffLED;
-import frc.robot.subsystems.limelight.TurnToTargetOpenLoop;
-import frc.robot.subsystems.limelight.TurnToTargetWithGyro;
-import frc.robot.subsystems.limelight.TurnToTargetClosedLoop;
+import frc.robot.subsystems.colorSensor.ColorSensorSubsystem;
 import frc.robot.subsystems.drive.ArcadeDrive;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 import frc.robot.subsystems.gyro.GyroSubsystem;
-import frc.robot.subsystems.limelight.FollowTarget;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
+import frc.robot.subsystems.limitswitch.LimitswitchSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
+
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
   private final XboxController joystick = new XboxController(0);
 
   private final LimelightSubsystem limelightSubsystem = new LimelightSubsystem();
   private final DriveBaseSubsystem driveBaseSubsystem = new DriveBaseSubsystem();
   private final GyroSubsystem gyroSubsystem = new GyroSubsystem();
+  private final ColorSensorSubsystem colorSensorSubsystem = new ColorSensorSubsystem();
+  private final LimitswitchSubsystem limitSwitchSubsystem = new LimitswitchSubsystem();
 
-  // private final TurnOffLED turnOffLED = new TurnOffLED(limelightSubsystem);
-  private final TurnToTargetClosedLoop turnToTargetClosedLoop = new TurnToTargetClosedLoop(driveBaseSubsystem, limelightSubsystem);
-  private final FollowTarget followTarget = new FollowTarget(driveBaseSubsystem, limelightSubsystem);
-  private final ArcadeDrive arcadeDrive = new ArcadeDrive(joystick, driveBaseSubsystem, 0.5, 0.5, 0.5, 0.5);
-  private final TurnToTargetWithGyro turnToTargetWithGyro = new TurnToTargetWithGyro(driveBaseSubsystem, limelightSubsystem, gyroSubsystem);
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the button bindings
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
   private void configureButtonBindings() {
    
-
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-//    *
-//    * @return the command to run in autonomous
-//    */
-
-  // uncomment when u need to use this
   public Command getAutonomousCommand() {
     return new WaitCommand(0);
   }
 
-  // schedule default commands here
   public void setDefaultCommands(){
 
   }
