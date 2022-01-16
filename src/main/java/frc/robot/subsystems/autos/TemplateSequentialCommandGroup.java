b787d33fdb489c0e7c971601c143b6e62e658675
Karan Saini <karansaini117@gmail.com>
2022-01-15 19:56:48 -0800

    added sequential and parallel cmd group templates with comments

---
+++ src/main/java/frc/robot/subsystems/autos/TemplateSequentialCommandGroup.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.autos;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class TemplateSequentialCommandGroup extends SequentialCommandGroup {
+
+  public TemplateSequentialCommandGroup() {
+    /* use:
+    addCommands(new FoodCommand());
+    addCommands(new BarCommand());
+    to add FooCommand and BarCommand to the sequential command group 
+    */
+
+
+  }
+
+}
