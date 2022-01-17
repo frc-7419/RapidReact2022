1e4da53ee42b3457a1b56da76ed7f5a26ff944a2
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2022-01-17 12:18:25 -0800

    Fixed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final PaddedXbox joystick = new PaddedXbox();
+  private final XboxController joystick = new XboxController(0);
   // private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
   
 
