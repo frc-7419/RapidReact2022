95765246011f1d47c97f3229d13e3c1dd98fac61
Karan Saini <karansaini117@gmail.com>
2022-01-17 12:44:43 -0800

    fix + merge

--- src/main/java/frc/robot/subsystems/potentiometer/PotentiometerSubsystem.java
+++ src/main/java/frc/robot/subsystems/potentiometer/PotentiometerSubsystem.java
@@ -2,13 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.ultrasonicSensor;
+package frc.robot.subsystems.potentiometer;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class UltrasonicDistanceSensorSubsystem extends SubsystemBase {
+public class PotentiometerSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public UltrasonicDistanceSensorSubsystem() {}
+  public PotentiometerSubsystem() {}
 
   @Override
   public void periodic() {
