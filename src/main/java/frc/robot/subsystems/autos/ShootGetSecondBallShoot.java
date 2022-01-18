dcaf9b8a111f17165330d9561dee43d09d9fb16f
Aarav Sharma <aaravlikestennis@gmail.com>
2022-01-18 09:58:12 -0800

    updated per. 6 auton file

--- src/main/java/frc/robot/subsystems/autos/ShootGetSecondBallShoot.java
+++ src/main/java/frc/robot/subsystems/autos/ShootGetSecondBallShoot.java
@@ -1,1 +1,13 @@
-//hello
+//period 6 file
+package frc.robot.subsystems.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class ShootGetSecondBallShoot extends SequentialCommandGroup {
+    
+    public ShootGetSecondBallShoot() { //add parameters
+        addCommands(new WaitCommand(0.5)); //this is how the auton code should look: a series of addCommands() functions 
+        //this is a comment
+    }
+}
