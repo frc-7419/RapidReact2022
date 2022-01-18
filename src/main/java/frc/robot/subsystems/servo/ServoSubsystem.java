86ca26eb5c6c5943c0c6b28e6c10118b17effc8a
Karan Saini <karansaini117@gmail.com>
2022-01-18 12:45:42 -0800

    added code to servo subsystem and setAngle() method

--- src/main/java/frc/robot/subsystems/servo/ServoSubsystem.java
+++ src/main/java/frc/robot/subsystems/servo/ServoSubsystem.java
@@ -1,14 +1,24 @@
 package frc.robot.subsystems.servo;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Servo;
+
+private Servo servo;
 
 public class ServoSubsystem extends SubsystemBase {
   
-  public ServoSubsystem() {
 
+  public ServoSubsystem() {
+    servo = new Servo(0); // put the specific PWM
   }
 
   @Override
   public void periodic() {
+
+  }
+
+  // Servo motors are controlled on a scale of 0.0 to 1.0
+  public void setAngle(double angle) {
+    servo.set(angle);
   }
 }
