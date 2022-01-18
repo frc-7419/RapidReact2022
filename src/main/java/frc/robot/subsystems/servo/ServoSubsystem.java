11d39d99c9eb4bb641dc1784cd5a6e2dc2b0765c
Karan Saini <karansaini117@gmail.com>
2022-01-18 12:24:30 -0800

    created servo folder and subsystem

---
+++ src/main/java/frc/robot/subsystems/servo/ServoSubsystem.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.servo;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ServoSubsystem extends SubsystemBase {
+  
+  public ServoSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+  }
+}
