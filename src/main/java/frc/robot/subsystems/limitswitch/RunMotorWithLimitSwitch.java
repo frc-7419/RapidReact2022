eaec5487eed86f9c6833c436546b7925f7bee5bc
Karan Saini <karansaini117@gmail.com>
2022-01-17 22:26:18 -0800

    quick adjustments and added suggestions for yall

--- src/main/java/frc/robot/subsystems/limitswitch/RunMotorWithLimitSwitch.java
+++ src/main/java/frc/robot/subsystems/limitswitch/RunMotorWithLimitSwitch.java
@@ -1,40 +1,40 @@
 package frc.robot.subsystems.limitswitch;
-// package frc.robot.subsystems.limitSwitch;
 
-// import edu.wpi.first.wpilibj2.command.CommandBase;
-// import frc.robot.subsystems.talon.TalonSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.talon.TalonSubsystem;
 
-// public class RunMotorWithLimitSwitch extends CommandBase {
-//   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-//   private LimitSwitchSubsystem limitSwitchSubsystem;
-//   private TalonSubsystem talonSubsystem;
+public class RunMotorWithLimitSwitch extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private LimitSwitchSubsystem limitSwitchSubsystem;
+  private TalonSubsystem talonSubsystem;
 
-//   public RunMotorWithLimitSwitch(LimitSwitchSubsystem limitSwitchSubsystem, TalonSubsystem talonSubsystem) {
-//     this.limitSwitchSubsystem = limitSwitchSubsystem;
-//     this.talonSubsystem= talonSubsystem;
-//     // uses addRequirements() instead of requires()
-//     addRequirements(limitSwitchSubsystem, talonSubsystem);
-//   }
+  public RunMotorWithLimitSwitch(LimitSwitchSubsystem limitSwitchSubsystem, TalonSubsystem talonSubsystem) {
+    this.limitSwitchSubsystem = limitSwitchSubsystem;
+    this.talonSubsystem = talonSubsystem;
+    // uses addRequirements() instead of requires()
+    addRequirements(limitSwitchSubsystem, talonSubsystem);
+  }
 
-//   @Override
-//   public void initialize() {
-//   }
+  @Override
+  public void initialize() {
+  }
 
-//   @Override
-//   public void execute() {
-//     if (limitSwitchSubsystem.getLimitSwitch().get()){
-//       talonSubsystem.setPower(0.2);
-//     }
-//     // add an else statement that brakes the motor
-//     else talonSubsystem.setPower(0);
-//   }
+  @Override
+  public void execute() {
+    if (limitSwitchSubsystem.getLimitSwitch().get()){
+      talonSubsystem.setPower(0.2);
+    }
+    // add an else statement that brakes the motor
+    else talonSubsystem.setPower(0);
+    // ^ this sets the power to 0 but doesn't BRAKE it. check assignments for help
+  }
 
-//   @Override
-//   public void end(boolean interrupted) {
-//   }
+  @Override
+  public void end(boolean interrupted) {
+  }
 
-//   @Override
-//   public boolean isFinished() {
-//     return false;
-//   }
-// }
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
