d9a1da39247d6d0deb737d0d8cb08b883d66bab9
Karan Saini <karansaini117@gmail.com>
2022-01-19 11:03:09 -0800

    fix warning

--- src/main/java/frc/robot/subsystems/limelight/TurnLimelightWithServoToTyOpenLoop.java
+++ src/main/java/frc/robot/subsystems/limelight/TurnLimelightWithServoToTyOpenLoop.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.limelight;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.LimelightConstants;
 import frc.robot.subsystems.servo.ServoSubsystem;
 
 public class TurnLimelightWithServoToTyOpenLoop extends CommandBase {
