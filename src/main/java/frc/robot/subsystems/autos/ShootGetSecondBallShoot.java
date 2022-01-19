4dc173758d0b804973f0aa7a0c5b0dac43013ac0
Keshav Badrinath <keshavbadrinath06@gmail.com>
2022-01-19 13:45:49 -0800

    added comments for basic structure of shooting, then getting second ball and shooting

--- src/main/java/frc/robot/subsystems/autos/ShootGetSecondBallShoot.java
+++ src/main/java/frc/robot/subsystems/autos/ShootGetSecondBallShoot.java
@@ -16,5 +16,45 @@ public class ShootGetSecondBallShoot extends SequentialCommandGroup {
         //Turn Command
         //ReadyToShoot
         //RunShooter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        //loadBallInShooter - load the next ball once we are ready to shoot
+        //runShooter - shoot ball with desired power and at the target, make sure to account for whether we are shooting into upper or lower hub
+        //adjust robot to face next ball to load
+        //use limelight to find the desired ball of our alliance's color
+        //use the move command to move the robot to the desired ball
+        //use the turn command to position the robot for loading the ball
+        //loadBallInShooter - load this ball into the shooter
+        //use limelight to detect the hubs, reposition robot
+        //runShooter - shoot ball with desired power and at the target, accounting for whether we are shooting into upper or lower hub
     }
 }
