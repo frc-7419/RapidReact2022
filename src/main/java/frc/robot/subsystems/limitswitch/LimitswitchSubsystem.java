0f7f8487aa7e5bbb41bc00038f339cbc97bc5a19
Karan Saini <karansaini117@gmail.com>
2022-01-18 23:09:54 -0800

    Update LimitswitchSubsystem.java

--- src/main/java/frc/robot/subsystems/limitswitch/LimitswitchSubsystem.java
+++ src/main/java/frc/robot/subsystems/limitswitch/LimitswitchSubsystem.java
@@ -9,7 +9,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//stephen was here
+
 public class LimitSwitchSubsystem extends SubsystemBase{
   private DigitalInput limitSwitch;
 
