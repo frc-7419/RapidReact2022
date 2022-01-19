527fa2ec87935544dc13704d25920c043d8e2bd9
Kaival145 <kaival.s.shah@gmail.com>
2022-01-19 14:09:00 -0800

    adding potentiometer code and some of robot container

--- src/main/java/frc/robot/subsystems/potentiometer/PotentiometerSubsystem.java
+++ src/main/java/frc/robot/subsystems/potentiometer/PotentiometerSubsystem.java
@@ -4,15 +4,22 @@
 
 package frc.robot.subsystems.potentiometer;
 
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PotentiometerSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public PotentiometerSubsystem() {}
+
+  private AnalogPotentiometer pot = new AnalogPotentiometer(0, 180, 30);
+  private double angle;
+  //30 is the starting point, 180 full range of motion
+
+  public PotentiometerSubsystem() {
+}
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // This method will be called oence per scheduler run
+    angle = pot.get();
   }
 
   @Override
