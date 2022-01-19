7ac67ed29ef7102b23125b60165e721de65314b0
Aarav Sharma <aaravlikestennis@gmail.com>
2022-01-19 13:41:35 -0800

    worked with keshav and charles to develop some comments for the auton path

--- src/main/java/frc/robot/subsystems/autos/ShootGetSecondBallShoot.java
+++ src/main/java/frc/robot/subsystems/autos/ShootGetSecondBallShoot.java
@@ -8,6 +8,13 @@ public class ShootGetSecondBallShoot extends SequentialCommandGroup {
     
     public ShootGetSecondBallShoot() { //add parameters
         addCommands(new WaitCommand(0.5)); //this is how the auton code should look: a series of addCommands() functions 
-        //this is a comment
+        //ReadyToShoot --> Load the ball into the shooter, basically get ready to shoot
+        //RunShooter --> Actually shoot the ball. Power should be set accordingly for lower hub/upper hub
+        //Turn robot to face one of the balls
+        //Use limelight to find ball of our alliance color
+        //Move Command to go to ball
+        //Turn Command
+        //ReadyToShoot
+        //RunShooter
     }
 }
