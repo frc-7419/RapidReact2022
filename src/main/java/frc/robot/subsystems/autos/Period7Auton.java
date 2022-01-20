2a99b7360fd022efc1a4ddbf9a94e4846b6bda57
poluk sharma <poluk.sharma@gmail.com>
2022-01-19 16:13:38 -0800

    Add tentative drive and turn values based on field measurements

--- src/main/java/frc/robot/subsystems/autos/Period7Auton.java
+++ src/main/java/frc/robot/subsystems/autos/Period7Auton.java
@@ -23,19 +23,19 @@ public class Period7Auton extends SequentialCommandGroup {
     // addCommands(new ShootAtAngle());
 
     // Turn to next cargo
-    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 10));
+    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, -136.5));
 
     // Drive forward to next cargo
-    addCommands(new StraightWithMotionMagic(driveBaseSubsystem,  0.5));
+    addCommands(new StraightWithMotionMagic(driveBaseSubsystem,  37.943));
 
     // Turn to the second cargo
-    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 10));
+    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, -32.25));
 
     // Drive forward to the second cargo
-    addCommands(new StraightWithMotionMagic(driveBaseSubsystem,  0.5));
+    addCommands(new StraightWithMotionMagic(driveBaseSubsystem,  107.664));
 
     // Turn roughly to the target
-    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 10));
+    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 80.25));
 
     // Use limight to adjust to the target
     // addCommands(new TurnToTx(driveBaseSubsystem, limelightSubsystem));
