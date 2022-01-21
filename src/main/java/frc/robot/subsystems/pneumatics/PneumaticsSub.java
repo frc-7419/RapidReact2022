c7fc8bfba32579e8813f35918d5518a48a9d2132
Aarav Sharma <aaravlikestennis@gmail.com>
2022-01-21 10:47:16 -0800

    fixed last error in PneumaticsSub

--- src/main/java/frc/robot/subsystems/pneumatics/PneumaticsSub.java
+++ src/main/java/frc/robot/subsystems/pneumatics/PneumaticsSub.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems.pneumatics;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PneumaticsSub extends SubsystemBase {
@@ -15,7 +16,7 @@ public class PneumaticsSub extends SubsystemBase {
   double current;
 
   public PneumaticsSub() {
-    c = new Compressor(0);
+    c = new Compressor(0, PneumaticsModuleType.CTREPCM);
     c.enableDigital();
     pressureSwitch = c.getPressureSwitchValue();
     current = c.getCurrent();
