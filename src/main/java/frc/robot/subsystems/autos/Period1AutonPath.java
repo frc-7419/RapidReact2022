6a02cfaa69fe3c8a92296bda41d443064b44e3b4
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-01-22 00:18:12 -0800

    Added auton steps to RIS Period 1 Auton sequential command group file

---
+++ src/main/java/frc/robot/subsystems/autos/Period1AutonPath.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class Period1AutonPath extends SequentialCommandGroup {
+    public Period1AutonPath() {
+        //addCommands(commands);
+        //Drive forward
+        //Intake cargo
+        //Drive backward
+        //Shoot both cargo (inc. preload)
+    }
+}
