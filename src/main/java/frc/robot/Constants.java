16dfa8fd7b12b10fa09edc95a2b3b88f5dcd3159
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2022-01-25 11:26:11 -0800

    tune pid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -60,7 +60,7 @@ public final class Constants {
 
     public static class PIDConstants {
         //gyro
-        public static double GyrokP = 0.085;
+        public static double GyrokP = 0.006;
         public static double GyrokI = 0;
         public static double GyrokD = 0;
     }
