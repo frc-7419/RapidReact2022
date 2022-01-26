3e511beb173efbedc73806eb8bd6ed7f4d6d2f34
SnehaG100 <email2sneha.g@gmail.com>
2022-01-25 16:03:38 -0800

    Update MotionMagicTest.java

--- src/main/java/frc/robot/subsystems/encoders/MotionMagicTest.java
+++ src/main/java/frc/robot/subsystems/encoders/MotionMagicTest.java
@@ -6,19 +6,32 @@ package frc.robot.subsystems.encoders;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import com.ctre.phoenix.motorcontrol.*;
 import com.ctre.phoenix.motorcontrol.can.*;
 
 public class MotionMagicTest extends CommandBase {
   /** Creates a new MotionMagicTest. */
-  
-  public MotionMagicTest() {
+  private TalonSubsystem talonSubsystem;
+  private double setpoint;
+
+  public MotionMagicTest(TalonSubsystem talonSubsystem, double setpoint) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.talonSubsystem = talonSubsystem;
+    this.setpoint = setpoint;
+    addRequirements(talonSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    SmartDashboard.putString("Status", "Starting Motion Magic Test");
+    talonSubsystem.setTalonPosition(0);
+    //0 is the timeoutMs
+    talonSubsystem.getTalon().configMotionCruiseVelocity(15000,0);
+    talonSubsystem.getTalon().configMotionCruiseVelocity(6000, 0);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
