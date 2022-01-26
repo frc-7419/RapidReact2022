2f0d2c7bbfdbf1e2a36df389dc2ce3f976810a40
Aarav Wattal <>
2022-01-25 16:51:04 -0800

    period 1 auton commands done

--- src/main/java/frc/robot/subsystems/autos/Period1AutonPath.java
+++ src/main/java/frc/robot/subsystems/autos/Period1AutonPath.java
@@ -1,9 +1,18 @@
 package frc.robot.subsystems.autos;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.drive.DriveBaseSubsystem;
+import frc.robot.subsystems.drive.StraightWithMotionMagic;
+import frc.robot.subsystems.gyro.TurnWithGyroClosedLoop;
 
 public class Period1AutonPath extends SequentialCommandGroup {
-    public Period1AutonPath() {
+
+    public Period1AutonPath(DriveBaseSubsystem driveBase, StraightWithMotionMagic straightWithMotionMagic, TurnWithGyroClosedLoop turnWithGyroClosedLoop) {
+        addCommands(new StraightWithMotionMagic(driveBase, 85.0));
+        addCommands(new WaitCommand(1));
+        addCommands(new StraightWithMotionMagic(driveBase, -85.0));
+        
         //addCommands(commands);
         //Drive forward
         //Intake cargo
