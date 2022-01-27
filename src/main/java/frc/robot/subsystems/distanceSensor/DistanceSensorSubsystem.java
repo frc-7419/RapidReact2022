dca7b59523b46e8087dfae2e79427aeeede25f6f
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-01-26 21:44:20 -0800

    Quick commit to save progress, added folder

---
+++ src/main/java/frc/robot/subsystems/distanceSensor/DistanceSensorSubsystem.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.distanceSensor;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DistanceSensorSubsystem extends SubsystemBase{
+    public DistanceSensorSubsystem() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+
+    }
+}
