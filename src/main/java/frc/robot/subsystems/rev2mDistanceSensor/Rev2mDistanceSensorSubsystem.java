33b6c33a9c031ee2d72203f201998e3083d39b16
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-01-28 14:37:00 -0800

    Fixed venderdep installation and maven merging and created a Rev2mDistanceSensor

--- src/main/java/frc/robot/subsystems/rev2mDistanceSensor/Rev2mDistanceSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/rev2mDistanceSensor/Rev2mDistanceSensorSubsystem.java
@@ -2,14 +2,17 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.ultrasonicSensor;
+package frc.robot.subsystems.rev2mDistanceSensor;
+
+import com.revrobotics.Rev2mDistanceSensor;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class UltrasonicDistanceSensorSubsystem extends SubsystemBase {
+public class Rev2mDistanceSensorSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
+  private Rev2mDistanceSensor distanceSensor;
   
-  public UltrasonicDistanceSensorSubsystem() {}
+  public Rev2mDistanceSensorSubsystem() {}
 
   @Override
   public void periodic() {
