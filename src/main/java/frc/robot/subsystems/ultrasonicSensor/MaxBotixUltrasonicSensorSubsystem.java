9f3e95e06b87e832ebc20f6d5720230d6b7df7a7
Aarav Sharma <aaravlikestennis@gmail.com>
2022-01-28 11:03:19 -0800

    added AnalogPotentiometer  (changed to Analog from Ultrasonic)

--- src/main/java/frc/robot/subsystems/ultrasonicSensor/MaxBotixUltrasonicSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ultrasonicSensor/MaxBotixUltrasonicSensorSubsystem.java
@@ -3,19 +3,30 @@ package frc.robot.subsystems.ultrasonicSensor;
 /* suggestion: import AnalogInput from wpilib, not revrobotics */
 import com.revrobotics.AnalogInput;
 
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class MaxBotixUltrasonicSensorSubsystem extends SubsystemBase {
-    private Ultrasonic ultrasonic;
-    
+    private AnalogPotentiometer pot;
+    private AnalogInput input;
+
     public MaxBotixUltrasonicSensorSubsystem() {
-        ultrasonic = new Ultrasonic(5,6);
+        //input = new AnalogInput(0); error --> cannot instanitate constructor
+        //input.setAverageBits(2)
+        pot = new AnalogPotentiometer(0,180,30);
     }
 
-    public void periodic() {
-        SmartDashboard.putNumber("distance: ", ultrasonic.getRangeInches());
-    }
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("distance: ", pot.get());
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
 }
