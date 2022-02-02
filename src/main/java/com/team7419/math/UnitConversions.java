c50d42b72f4b532701bcc594c5725cfb7fcc59fb
Karan Saini <karansaini117@gmail.com>
2022-02-01 22:24:54 -0800

    adding unit conv for rawSensorVelocityToRPM

--- src/main/java/com/team7419/math/UnitConversions.java
+++ src/main/java/com/team7419/math/UnitConversions.java
@@ -6,12 +6,18 @@ public class UnitConversions{
         return rpm * 2 * Math.PI / 60;
     }
 
-    public static double mPSToRPM(double mps, double radius) {
+    public static double mPSToRPM(double mps, double radius) { // m is meters
         return (60*mps)/(2*Math.PI*radius);
     }
 
-    public static int mPSToTicksP100Ms(double input){ // m is meters
+    public static int mPSToTicksP100Ms(double input) { // m is meters
 		double output = (((input*39.3701)/6)*2048)/10;
 		return (int) Math.round(output);
 	}
+
+    
+
+    public static rawSensorVelocityToRPM(double rawVelocity) {
+        return 
+    }
 }
