174516f2f4d18ff580cd47fffca6041ce36f9f4c
Karan Saini <karansaini117@gmail.com>
2022-02-02 21:57:30 -0800

    update

--- src/main/java/com/team7419/math/UnitConversions.java
+++ src/main/java/com/team7419/math/UnitConversions.java
@@ -1,13 +1,17 @@
 package com.team7419.math;
 
-public class UnitConversions{
+public class UnitConversions {
 
-    public static double rpmToRadPerSec(double rpm) {
+    public static double rpmToRadPerSec(double rpm){
         return rpm * 2 * Math.PI / 60;
     }
 
-    public static double mPSToRPM(double mps, double radius) {
-        return (60*mps)/(2*Math.PI*radius);
+    public static double mpsToRPM(double mps, double radius) { // m is meters
+        return (60 * mps)/(2 * Math.PI * radius);
+    }
+
+    public static double rpmToMPS(double rpm, double radius) {
+        return (2 * Math.PI * radius) / 60;
     }
 
     public static int mPSToTicksP100Ms(double input) { // m is meters
@@ -15,17 +19,31 @@ public class UnitConversions{
 		return (int) Math.round(output);
 	}
 
+    public static double inchesToMeters(double inches) {
+        return inches * 0.0254;
+    }
+
+    public static double metersToInches(double meters) { 
+        return meters * 39.3701;
+    }
+
+    public static double rawSensorVelocityToRPM(double rawVelocity, double ticksPerRotation) {
+        return rawVelocity * (1/ticksPerRotation) * 600;
+    }
+
+    public static double rpmToRawSensorVelocity(double rpm, double radius, double gearRatioMultiplier, double ticksPerRotation) {
+        return rpm * (2 * Math.PI * radius) * (1/ticksToInches(1, radius, gearRatioMultiplier, ticksPerRotation)) * (60/0.1);
+    }
+
     public static double inchesToTicks(double inches, double radius, double gearRatioMultiplier, double ticksPerRotation) {
-        //(ticks per rotation/diameter of wheels)*inches
         return (ticksPerRotation * inches * gearRatioMultiplier)/(2 * Math.PI * radius);
     }
 
     public static double ticksToInches(double ticks, double radius, double gearRatioMultiplier, double ticksPerRotation) {
         return (2 * Math.PI * radius * ticks)/(ticksPerRotation * gearRatioMultiplier);
     }
+
     public static double thetaToInches(double theta, double radius) {
-        // convert theta to arc length using radius
         return theta * (Math.PI/180) * radius;
     }
-    
 }
