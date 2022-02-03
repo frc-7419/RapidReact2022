497a7dcd2e472229f8c82f94d47783a02cfad0cf
SiddharthR06 <93623443+SiddharthR06@users.noreply.github.com>
2022-02-03 12:28:24 -0800

    Create Period2Auton.java
    Period2Auton

---
+++ src/main/java/frc/robot/subsystems/autos/Period2Auton.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.autos;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drive.DriveBaseSubsystem;
+import frc.robot.subsystems.drive.StraightWithMotionMagic;
+import frc.robot.subsystems.gyro.GyroSubsystem;
+import frc.robot.subsystems.gyro.TurnWithGyroClosedLoop;
+
+public class Period2Auton extends SequentialCommandGroup {
+
+  public Period2Auton(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem) {
+
+
+
+  }
+
+}
