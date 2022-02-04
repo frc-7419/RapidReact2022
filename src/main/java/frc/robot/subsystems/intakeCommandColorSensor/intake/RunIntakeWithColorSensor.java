cbc81146328358fcd12e9ed14df1c0a68fcda61e
Keshav Badrinath <keshavbadrinath06@gmail.com>
2022-02-04 11:00:43 -0800

    power stuffs

--- src/main/java/frc/robot/subsystems/intakeCommandColorSensor/intake/RunIntakeWithColorSensor.java
+++ src/main/java/frc/robot/subsystems/intakeCommandColorSensor/intake/RunIntakeWithColorSensor.java
@@ -5,15 +5,18 @@
 package frc.robot.subsystems.intakeCommandColorSensor.intake;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.PowerConstants;
 import frc.robot.subsystems.intakeCommandColorSensor.colorSensor.ColorSensorSubsystem;
 
 public class RunIntakeWithColorSensor extends CommandBase {
   private ColorSensorSubsystem colorSensorSubsystem;
   private IntakeSubsystem intakeSubsystem;
+  private double power;
   /** Creates a new RunIntakeWithColorSensor. */
-  public RunIntakeWithColorSensor(IntakeSubsystem intakeSubsystem, ColorSensorSubsystem colorSensorSubsystem) {
+  public RunIntakeWithColorSensor(IntakeSubsystem intakeSubsystem, ColorSensorSubsystem colorSensorSubsystem, double power) {
     this.intakeSubsystem = intakeSubsystem;
     this.colorSensorSubsystem = colorSensorSubsystem;
+    this.power = power;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(intakeSubsystem);
     addRequirements(colorSensorSubsystem);
@@ -21,7 +24,9 @@ public class RunIntakeWithColorSensor extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    intakeSubsystem.setPower(power);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -29,7 +34,9 @@ public class RunIntakeWithColorSensor extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    intakeSubsystem.setPower(0);
+  }
 
   // Returns true when the command should end.
   @Override
