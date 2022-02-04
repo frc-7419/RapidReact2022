172293647792622a3935b99d1695126f37f5329e
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2022-02-04 12:33:28 -0800

    New joystick sub

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,17 +7,19 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.shooter.ShooterSubsystem;
+import frc.robot.subsystems.shooter.ArrowRunShooterWithJoystick;
+import frc.robot.subsystems.shooter.BasicShooterSubsystem;
 import frc.robot.Constants.PIDConstants;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.shooter.GetToTargetVelocity;
 
 public class RobotContainer {
   private final XboxController joystick = new XboxController(0);
-  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+  private final BasicShooterSubsystem basicShooterSubsystem = new BasicShooterSubsystem();
   
-  private final GetToTargetVelocity getToTargetVelocity = new GetToTargetVelocity(shooterSubsystem, 1000);
+  private final ArrowRunShooterWithJoystick arrowRunShooterWithJoystick = new ArrowRunShooterWithJoystick(basicShooterSubsystem, joystick);
 
   public RobotContainer() {
     configureButtonBindings();
@@ -26,7 +28,7 @@ public class RobotContainer {
   }
 
   private void configureButtonBindings() {
-    new JoystickButton(joystick, XboxController.Button.kY.value).toggleWhenPressed(new GetToTargetVelocity(shooterSubsystem, 1000));
+    // new JoystickButton(joystick, XboxController.Button.kY.value).toggleWhenPressed(new GetToTargetVelocity(shooterSubsystem, 1000));
    
   }
 
@@ -37,10 +39,11 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return getToTargetVelocity;
+    return new WaitCommand(0);
   }
 
   public void setDefaultCommands(){
     // shooterBasicSubsystem.setDefaultCommand(runShooterWithJoystick);
+    basicShooterSubsystem.setDefaultCommand(arrowRunShooterWithJoystick);
   }
 }
