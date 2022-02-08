3b26839489531b542a30882615683cb7820d077e
Ricky Hu <rhustudy@gmail.com>
2022-02-07 16:32:58 -0800

    Attempted to merge two camera streams into a single one

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,13 @@
 
 package frc.robot;
 
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.XboxController;
@@ -24,6 +31,40 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    // CameraServer setup
+    new Thread(() -> {
+      UsbCamera cam1 = CameraServer.startAutomaticCapture();
+      UsbCamera cam2 = CameraServer.startAutomaticCapture();
+      CvSink video1 = CameraServer.getVideo(cam1);
+      CvSink video2 = CameraServer.getVideo(cam2);
+      cam1.setResolution(400, 320);
+      cam2.setResolution(400, 320);
+      CvSource combined = CameraServer.putVideo("Front view", 800, 320); //edit these values later
+
+      Mat s1 = new Mat();
+      Mat s2 = new Mat();
+      Mat o = new Mat();
+      byte[] rgb1;
+      byte[] rgb2;
+
+      while (!Thread.interrupted()) {
+        if (video1.grabFrame(s1)==0 || video2.grabFrame(s2)==0) {
+          continue;
+        }
+        rgb1 = new byte[3];
+        rgb2 = new byte[3];
+        for (int row=0;row<320;row++) {
+          for (int col=0;col<400;col++) {
+            s1.get(row, col, rgb1);
+            s2.get(row, col, rgb2);
+            o.put(row, col, rgb1);
+            o.put(row, col+400, rgb2);
+          }
+        }
+        combined.putFrame(o);
+      }
+
+    }).start();
     // Configure the button bindings
     configureButtonBindings();
   }
