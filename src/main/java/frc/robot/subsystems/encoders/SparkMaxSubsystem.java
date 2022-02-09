d96cad8848bcc34d00293117c4afb7580867faa2
Keshav Badrinath <keshavbadrinath06@gmail.com>
2022-02-09 13:57:04 -0800

    added limit switch and smart dashboard puts

--- src/main/java/frc/robot/subsystems/encoders/SparkMaxSubsystem.java
+++ src/main/java/frc/robot/subsystems/encoders/SparkMaxSubsystem.java
@@ -7,19 +7,31 @@ package frc.robot.subsystems.encoders;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SparkMaxSubsystem extends SubsystemBase {
   /** Creates a new SparkMaxSubsystem. */
   private CANSparkMax canSparkMax;
-
+  private DigitalInput limitSwitch;
   public SparkMaxSubsystem() {
     canSparkMax = new CANSparkMax(0, MotorType.kBrushless);
+    limitSwitch = new DigitalInput(0);
     canSparkMax.restoreFactoryDefaults();
   }
-
+  public DigitalInput getLimitSwitch() {
+    return limitSwitch;
+  }
+  public boolean get() {
+    return limitSwitch.get();
+  }
+  public void setSpeed(double speed) {
+    canSparkMax.set(speed);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putBoolean("Limit Switch", limitSwitch.get());
   }
 }
