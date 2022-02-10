44c039eac2add223ac5d90b825e9411701ba6db0
Keshav Badrinath <keshavbadrinath06@gmail.com>
2022-02-09 17:03:59 -0800

    cleaned code

--- src/main/java/frc/robot/subsystems/encoders/RunSparkMaxWithLimitSwitch.java
+++ src/main/java/frc/robot/subsystems/encoders/RunSparkMaxWithLimitSwitch.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems.encoders;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
