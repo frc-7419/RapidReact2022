c397f8df177285d0b8a71a9ebfcb5a3907e270c3
Kaival145 <kaival.s.shah@gmail.com>
2022-02-12 14:07:55 -0800

    Add joystick

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.encoders.RunSparkMax;
+import frc.robot.subsystems.encoders.JoystickSparkMax;
 import frc.robot.subsystems.encoders.SparkMaxSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -22,7 +24,8 @@ public class RobotContainer {
   // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final XboxController joystick = new XboxController(0);
   private final SparkMaxSubsystem sparkSubsystem = new SparkMaxSubsystem();
-  private final RunSparkMax runSparkMax = new RunSparkMax(sparkSubsystem);
+// private final RunSparkMax runSparkMax = new RunSparkMax(sparkSubsystem);
+  private final JoystickSparkMax joystickSparkMax = new JoystickSparkMax(sparkSubsystem, joystick);
   // private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
   
 
@@ -48,12 +51,12 @@ public class RobotContainer {
 //    */
 
   // uncomment when u need to use this
-  // public Command getAutonomousCommand() {
-  //   return autonomousCommand;
-  // }
+  public Command getAutonomousCommand() {
+    return new WaitCommand(0);
+  }
 
   // set default commands here
   public void setDefaultCommands(){
-    sparkSubsystem.setDefaultCommand(runSparkMax);
+    sparkSubsystem.setDefaultCommand(joystickSparkMax);
   }
 }
--- src/main/java/frc/robot/subsystems/encoders/JoystickSparkMax.java
+++ src/main/java/frc/robot/subsystems/encoders/JoystickSparkMax.java
@@ -6,16 +6,19 @@ package frc.robot.subsystems.encoders;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class RunSparkMax extends CommandBase {
+public class JoystickSparkMax extends CommandBase {
   /** Creates a new RunSparkMax. */
   private SparkMaxSubsystem spark;
-  public RunSparkMax(SparkMaxSubsystem spark) {
+  private XboxController joystick;
+  public JoystickSparkMax(SparkMaxSubsystem spark, XboxController joystick) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.spark = spark;
+    this.joystick = joystick;
     addRequirements(spark);
   }
 
@@ -26,9 +29,9 @@ public class RunSparkMax extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    spark.getSoftLimitForward();
-    spark.getSoftLimitReverse();
-    spark.setSpeed(0.2);
+    if (joystick.getAButtonPressed()){
+        spark.setSpeed(1);
+    }
   }
 
   // Called once the command ends or is interrupted.
