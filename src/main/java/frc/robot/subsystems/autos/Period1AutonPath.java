c0c2a8e0268c0354b452c68fd0c1a50216c75b25
AaravWattal <61303690+AaravWattal@users.noreply.github.com>
2022-02-17 10:28:15 -0800

    period 1 auton commands (180 degree turn)

--- src/main/java/frc/robot/subsystems/autos/Period1AutonPath.java
+++ src/main/java/frc/robot/subsystems/autos/Period1AutonPath.java
@@ -1,17 +1,19 @@
 package frc.robot.subsystems.autos;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.PIDConstants;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 import frc.robot.subsystems.drive.StraightWithMotionMagic;
+import frc.robot.subsystems.gyro.GyroSubsystem;
 import frc.robot.subsystems.gyro.TurnWithGyroClosedLoop;
-
 public class Period1AutonPath extends SequentialCommandGroup {
 
-    public Period1AutonPath(DriveBaseSubsystem driveBase) {
-        addCommands(new StraightWithMotionMagic(driveBase, 85.0));
-        addCommands(new WaitCommand(1));
-        addCommands(new StraightWithMotionMagic(driveBase, -85.0));
+    public Period1AutonPath(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem) {
+        addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 85.0));
+        addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 180, PIDConstants.GyrokP180, PIDConstants.GyrokI180, PIDConstants.GyrokD180));
+        addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 85.0));
         
         //addCommands(commands);
         //Drive forward
