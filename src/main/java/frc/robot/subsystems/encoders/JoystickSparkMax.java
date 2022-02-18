a53d2893346c1893a44114d5359255d0ca4412b2
Karan Saini <karansaini117@gmail.com>
2022-02-18 13:01:27 -0800

    changed NC

--- src/main/java/frc/robot/subsystems/encoders/JoystickSparkMax.java
+++ src/main/java/frc/robot/subsystems/encoders/JoystickSparkMax.java
@@ -4,22 +4,18 @@
 
 package frc.robot.subsystems.encoders;
 
-import com.revrobotics.CANSparkMax;
-
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class JoystickSparkMax extends CommandBase {
   /** Creates a new RunSparkMax. */
-  private SparkMaxSubsystem spark;
+  private SparkMaxSubsystem sparkMaxSubsystem;
   private XboxController joystick;
-  public JoystickSparkMax(SparkMaxSubsystem spark, XboxController joystick) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.spark = spark;
+
+  public JoystickSparkMax(SparkMaxSubsystem sparkMaxSubsystem, XboxController joystick) {
+    this.sparkMaxSubsystem = sparkMaxSubsystem;
     this.joystick = joystick;
-    addRequirements(spark);
+    addRequirements(sparkMaxSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -29,7 +25,7 @@ public class JoystickSparkMax extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    spark.setPower(joystick.getRightY());
+    sparkMaxSubsystem.setPower(joystick.getRightY());
   }
 
   // Called once the command ends or is interrupted.
