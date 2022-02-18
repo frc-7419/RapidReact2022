490b99755fd614a168ee38bc648ca0ab77d615ba
Aarav Sharma <aaravlikestennis@gmail.com>
2022-02-18 11:00:35 -0800

    no message

---
+++ src/main/java/frc/robot/subsystems/limitswitch/LimitSwitchSubsystem.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.limitswitch;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimitSwitchSubsystem extends SubsystemBase {
+  private DigitalInput limitSwitch;
+  
+  public LimitSwitchSubsystem() {
+    limitSwitch = new DigitalInput(0);
+  }
+
+  public DigitalInput getLimitSwitch() {
+    return limitSwitch;
+  }
+
+
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putBoolean("limit switch: ", limitSwitch.get());
+  }
+}
