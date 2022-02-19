70f9bc93b4e84f94a5aafa7de6dcb9e80f430c72
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-02-18 16:21:42 -0800

    Changed PID Constant–slightly working

--- src/main/java/frc/robot/subsystems/autos/Period1ThreeBallClose.java
+++ src/main/java/frc/robot/subsystems/autos/Period1ThreeBallClose.java
@@ -28,15 +28,15 @@ public class Period1ThreeBallClose extends SequentialCommandGroup {
     // go back out of the tarmac
     */
 
-    // addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 52)); 
-    // addCommands(new WaitCommand(0.2));
-    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 58, PIDConstants.GyrokP58, PIDConstants.GyrokI58, PIDConstants.GyrokD58));// turn 58 degrees
-    // addCommands(new WaitCommand(0.2));
-    // addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 113));
-    // addCommands(new WaitCommand(0.2));
-    // addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 58, PIDConstants.GyrokP58, PIDConstants.GyrokI58, PIDConstants.GyrokD58));// turn 58 degrees
-    // addCommands(new WaitCommand(0.2));
-    // addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 52));
+    addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 52)); 
+    addCommands(new WaitCommand(0.2));
+    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, -58, PIDConstants.GyrokP58, PIDConstants.GyrokI58, PIDConstants.GyrokD58));// turn 58 degrees
+    addCommands(new WaitCommand(0.2));
+    addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 113));
+    addCommands(new WaitCommand(0.2));
+    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, -58, PIDConstants.GyrokP58, PIDConstants.GyrokI58, PIDConstants.GyrokD58));// turn 58 degrees
+    addCommands(new WaitCommand(0.2));
+    addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 52));
   }
 
 }
