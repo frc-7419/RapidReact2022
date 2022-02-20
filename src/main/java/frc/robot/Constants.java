b5371ae8eb003b269fc49a535c681514a12c7252
DESKTOP-FIJ0Q55\Tech Support <qls7419leadership@gmail.com>
2022-02-19 17:47:57 -0800

    top / bottom can ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,8 +30,8 @@ public final class Constants {
         leftFalcon2(4),
         rightFalcon2(3),
         intakeVictor(11),
-        topShooterFalcon(10), 
-        bottomShooterFalcon(13),
+        topShooterFalcon(14), 
+        bottomShooterFalcon(10),
         hoodVictor(40), 
 
         //2022 robot constants
