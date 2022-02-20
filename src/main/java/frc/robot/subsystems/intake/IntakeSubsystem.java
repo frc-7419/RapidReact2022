1285e98b9117d38a7bddb5ff29747a92b5a0d399
Karan Saini <karansaini117@gmail.com>
2022-02-19 20:43:42 -0800

    removed pneumatics stuff from intake sub (only motor should be in there)

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -9,38 +9,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CanIds;
 
 public class IntakeSubsystem extends SubsystemBase{
-    private CANSparkMax intakeMotor;
-    private Solenoid intakeSolenoid; 
+    private CANSparkMax intake;
 
     public IntakeSubsystem() {
-        intakeMotor = new CANSparkMax(CanIds.intake.id, MotorType.kBrushless);
-        intakeSolenoid = new Solenoid(1, PneumaticsModuleType.CTREPCM, 0);
+        intake = new CANSparkMax(CanIds.intake.id, MotorType.kBrushless);
     }
 
     @Override
     public void periodic() {}
 
     public void setPower(double power) {
-        intakeMotor.set(power);
-    }
-
-    public void actuateSolenoid() {
-        intakeSolenoid.set(true);
-    }
-    
-    public void retractSolenoid() {
-        intakeSolenoid.set(false);
-    }
-
-    public void toggleSolenoid() {
-        intakeSolenoid.toggle();
-    }
-
-    public Solenoid getIntakeSolenoid() {
-        return intakeSolenoid;
+        intake.set(power);
     }
 
     public CANSparkMax getIntakeMotor() {
-        return intakeMotor;
+        return intake;
     } 
 }
