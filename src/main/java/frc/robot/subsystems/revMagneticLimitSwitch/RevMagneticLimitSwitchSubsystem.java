b80a16383bb170a56ed76be700e33f760e57361c
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-02-22 13:20:47 -0800

    Created subsystem

---
+++ src/main/java/frc/robot/subsystems/revMagneticLimitSwitch/RevMagneticLimitSwitchSubsystem.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.revMagneticLimitSwitch;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class RevMagneticLimitSwitchSubsystem extends SubsystemBase {
+  /** Creates a new RevMagneticLimitSwitchSubsystem. */
+  DigitalInput revMagneticLimitSwitch;
+  public RevMagneticLimitSwitchSubsystem() {
+    this.revMagneticLimitSwitch = new DigitalInput(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
