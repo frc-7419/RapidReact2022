46c1856a1746538cb9215ebcb794c5564cbbf427
Keshav Badrinath <keshavbadrinath06@gmail.com>
2022-02-22 09:54:35 -0800

    removed unnecessary imports

--- src/main/java/frc/robot/subsystems/encoders/SparkMaxSubsystem.java
+++ src/main/java/frc/robot/subsystems/encoders/SparkMaxSubsystem.java
@@ -8,8 +8,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxLimitSwitch;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
