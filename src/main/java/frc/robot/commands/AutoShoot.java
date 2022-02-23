c7be80a6bd8a07aa232b8102be7f7350c6697e35
Karan Saini <karansaini117@gmail.com>
2022-02-23 01:20:06 -0800

    finished (ish)

--- src/main/java/frc/robot/commands/AutoShoot.java
+++ src/main/java/frc/robot/commands/AutoShoot.java
@@ -6,16 +6,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.limelight.LimelightSubsystem;
 import frc.robot.subsystems.loader.LoaderSubsystem;
 import frc.robot.subsystems.loader.RunLoader;
+import frc.robot.subsystems.shooter.GetToTargetVelocity;
+import frc.robot.subsystems.shooter.GetToTargetVelocityWithLimelight;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.transferWheel.RunTransferWheel;
 import frc.robot.subsystems.transferWheel.TransferWheelSubsystem;
 import frc.robot.subsystems.turret.TurretSubsystem;
 
 public class AutoShoot extends SequentialCommandGroup {
-  /** Creates a new AutoShoot. */
-  public AutoShoot(LoaderSubsystem loaderSubsystem, TransferWheelSubsystem transferWheelSubsystem, TurretSubsystem turretSubsystem, ShooterSubsystem shooterSubsystem) {
-    addCommands(new ParallelCommandGroup(new RunLoader(loaderSubsystem, 1), new RunTransferWheel(transferWheelSubsystem, 1)));
+  public AutoShoot(LoaderSubsystem loaderSubsystem, TransferWheelSubsystem transferWheelSubsystem, TurretSubsystem turretSubsystem, ShooterSubsystem shooterSubsystem, LimelightSubsystem limelightSubsystem) {
+    addCommands(new GetToTargetVelocityWithLimelight(shooterSubsystem, limelightSubsystem).withInterrupt(shooterSubsystem::bothOnTarget));
+    addCommands(new ParallelCommandGroup(new GetToTargetVelocityWithLimelight(shooterSubsystem, limelightSubsystem)), new RunLoader(loaderSubsystem, 0.5), new RunTransferWheel(transferWheelSubsystem, 1));
   }
 }
