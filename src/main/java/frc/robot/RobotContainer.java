2f0b1a2a97fb61b3cc54acbbc63f7fc23914abb2
Sidharth Anand <sid@sidharthanand.com>
2022-02-22 16:24:49 -0800

    basic pathing test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,31 @@
 
 package frc.robot;
 
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj.XboxController;  
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants.CanIds;
+import frc.robot.Constants.TrajectoryConstants;
+import frc.robot.subsystems.drive.DifferentialDriveSubsystem;
 // import frc.robot.subsystems.autos.TemplateSequentialCommandGroup;
 // import frc.robot.subsystems.dashboard.Dashboard;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 // import frc.robot.subsystems.drive.StraightWithMotionMagic;
 import frc.robot.subsystems.gyro.GyroSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -27,6 +43,7 @@ public class RobotContainer {
   
   private final DriveBaseSubsystem driveBaseSubsystem = new DriveBaseSubsystem();
   private final GyroSubsystem gyro = new GyroSubsystem();
+  private final DifferentialDriveSubsystem m_robotDrive = new DifferentialDriveSubsystem();
 
   // instantiate your auto commands here
   // private final StraightWithMotionMagic straightWithMotionMagic = new StraightWithMotionMagic(driveBaseSubsystem, 12);
@@ -54,10 +71,63 @@ public class RobotContainer {
 //    */
 
   // uncomment when u need to use this
-  // public Command getAutonomousCommand() {
+  public Command getAutonomousCommand() {
     
-  //   // return straightWithMotionMagic;
-  // }
+        // Create a voltage constraint to ensure we don't accelerate too fast
+        var autoVoltageConstraint =
+        new DifferentialDriveVoltageConstraint(
+            new SimpleMotorFeedforward(
+                TrajectoryConstants.ks,
+                TrajectoryConstants.kv,
+                TrajectoryConstants.ka),
+            TrajectoryConstants.kDriveKinematics,
+            10);
+
+    // Create config for trajectory
+    TrajectoryConfig config =
+        new TrajectoryConfig(
+                TrajectoryConstants.kMaxSpeed,
+                TrajectoryConstants.kMaxAcceleration)
+            // Add kinematics to ensure max speed is actually obeyed
+            .setKinematics(TrajectoryConstants.kDriveKinematics)
+            // Apply the voltage constraint
+            .addConstraint(autoVoltageConstraint);
+
+    // An example trajectory to follow.  All units in meters.
+    Trajectory exampleTrajectory =
+        TrajectoryGenerator.generateTrajectory(
+            // Start at the origin facing the +X direction
+            new Pose2d(0, 0, new Rotation2d(0)),
+            // Pass through these two interior waypoints, making an 's' curve path
+            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+            // End 3 meters straight ahead of where we started, facing forward
+            new Pose2d(3, 0, new Rotation2d(0)),
+            // Pass config
+            config);
+
+    RamseteCommand ramseteCommand =
+        new RamseteCommand(
+            exampleTrajectory,
+            m_robotDrive::getPose,
+            new RamseteController(TrajectoryConstants.kRamseteB, TrajectoryConstants.kRamseteZeta),
+            new SimpleMotorFeedforward(
+                TrajectoryConstants.ks,
+                TrajectoryConstants.kv,
+                TrajectoryConstants.ka),
+            TrajectoryConstants.kDriveKinematics,
+            m_robotDrive::getWheelSpeeds,
+            new PIDController(TrajectoryConstants.kPDriveVel, 0, 0),
+            new PIDController(TrajectoryConstants.kPDriveVel, 0, 0),
+            // RamseteCommand passes volts to the callback
+            m_robotDrive::tankDriveVolts,
+            m_robotDrive);
+
+    // Reset odometry to the starting pose of the trajectory.
+    m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // Run path following command, then stop at the end.
+    return ramseteCommand.andThen(() -> m_robotDrive.tankDriveVolts(0, 0));
+  }
 
   // schedule default commands here
   public void setDefaultCommands(){
