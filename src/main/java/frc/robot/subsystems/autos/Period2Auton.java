426e39f5883fc2e06be1b69a769664413aff1446
SnehaG100 <email2sneha.g@gmail.com>
2022-02-24 11:51:55 -0800

    Added non driving commands to code as comments

--- src/main/java/frc/robot/subsystems/autos/Period2Auton.java
+++ src/main/java/frc/robot/subsystems/autos/Period2Auton.java
@@ -12,21 +12,23 @@ import frc.robot.subsystems.gyro.TurnWithGyroClosedLoop;
 public class Period2Auton extends SequentialCommandGroup {
 
   public Period2Auton(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem) {
+    //Turns robot towards the hub
     SmartDashboard.putString("command running", "thirty degrees");
     addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 30, PIDConstants.GyrokP30, PIDConstants.GyrokI30, PIDConstants.GyrokD30));
-    // //addCommands(shootBall());
+    //addCommands(shootBall());
     SmartDashboard.putString("command running", "-49 deg");
     addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, -49, PIDConstants.GyrokPNegative49, PIDConstants.GyrokINegative49, PIDConstants.GyrokDNegative49));
     SmartDashboard.putString("command running", "going back");
     addCommands(new StraightWithMotionMagic(driveBaseSubsystem, -40.375));
     // //addCommands(new collectBall());
     // //addCommands(new shootBall());
-    SmartDashboard.putString("command running", "-49 degrees");
-
+    SmartDashboard.putString("command running", "115 degrees");
     addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 115, PIDConstants.GyrokP115, PIDConstants.GyrokI115, PIDConstants.GyrokD115));
     SmartDashboard.putString("command running", "going forward");
     addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 107));
+    //addCommands(new collectBall());
     addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 80, PIDConstants.GyrokP80, PIDConstants.GyrokI80, PIDConstants.GyrokD80));
+    //addCommands(new shootBall());
 
 
   }
