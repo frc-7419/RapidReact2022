956e395fc2b3a551983f36799899cb60a05339be
Aarav Sharma <aaravlikestennis@gmail.com>
2022-02-25 10:54:05 -0800

    created new file for 4-ball auton

---
+++ src/main/java/frc/robot/subsystems/autos/FourBallAutonPath.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.autos;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class FourBallAutonPath extends SequentialCommandGroup {
+
+  public FourBallAutonPath() {
+    //use addCommands()
+  }
+}
