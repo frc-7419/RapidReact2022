96382500e2f300a9cc5d9d8a484001489135f974
braden0m <95321149+braden0m@users.noreply.github.com>
2022-03-01 15:17:59 -0800

    run intake

--- src/main/java/frc/robot/subsystems/autos/BetterPeriod2Auton.java
+++ src/main/java/frc/robot/subsystems/autos/BetterPeriod2Auton.java
@@ -11,6 +11,7 @@ import frc.robot.subsystems.gyro.GyroSubsystem;
 import frc.robot.subsystems.gyro.TurnWithGyroClosedLoop;
 import frc.robot.subsystems.intake.DeployIntake;
 import frc.robot.subsystems.intake.IntakeSolenoidSubsystem;
+import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.RunIntake;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
 import frc.robot.subsystems.shooter.GetToTargetVelocity;
@@ -21,9 +22,9 @@ import frc.robot.subsystems.turret.TurretSubsystem;
 
 public class BetterPeriod2Auton extends SequentialCommandGroup {
 
-  public BetterPeriod2Auton(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, TurretSubsystem turretSubsystem, LimelightSubsystem limelightSubsystem, ShooterSubsystem shooterSubsystem, FeederSubsystem feederSubsystem, IntakeSolenoidSubsystem intakeSolenoidSubsystem) {
+  public BetterPeriod2Auton(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, TurretSubsystem turretSubsystem, LimelightSubsystem limelightSubsystem, ShooterSubsystem shooterSubsystem, FeederSubsystem feederSubsystem, IntakeSubsystem intakeSubsystem) {
     //DeployIntake command will be replaced with the RunIntake command once it is fixed
-    addCommands(parallel(new Period2Auton(driveBaseSubsystem, gyroSubsystem, turretSubsystem, limelightSubsystem, shooterSubsystem, feederSubsystem), new DeployIntake(intakeSolenoidSubsystem)));
+    addCommands(parallel(new Period2Auton(driveBaseSubsystem, gyroSubsystem, turretSubsystem, limelightSubsystem, shooterSubsystem, feederSubsystem), new RunIntake(intakeSubsystem, 0.3)));
   }
 
 }
