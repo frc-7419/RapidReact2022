1a1ff5d9e5cfd5f0b1d1ecc877932163c8c30191
Karan Saini <karansaini117@gmail.com>
2022-02-28 20:29:17 -0800

    Update ShootThenMoveAway.java

--- src/main/java/frc/robot/subsystems/autos/ShootThenMoveAway.java
+++ src/main/java/frc/robot/subsystems/autos/ShootThenMoveAway.java
@@ -8,12 +8,12 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.PIDConstants;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 import frc.robot.subsystems.drive.StraightWithMotionMagic;
+import frc.robot.subsystems.feeder.FeederSubsystem;
 import frc.robot.subsystems.gyro.GyroSubsystem;
 import frc.robot.subsystems.gyro.TurnWithGyroClosedLoop;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
 import frc.robot.subsystems.shooter.GetToTargetVelocityWithLimelight;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
-import frc.robot.subsystems.transferWheel.TransferWheelSubsystem;
 import frc.robot.subsystems.turret.TurretSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -21,9 +21,9 @@ import frc.robot.subsystems.turret.TurretSubsystem;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ShootThenMoveAway extends SequentialCommandGroup {
   /** Creates a new ShootThenMoveAway. */
-  public ShootThenMoveAway(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, ShooterSubsystem shooterSubsystem, LimelightSubsystem limelightSubsystem, TurretSubsystem turretSubsystem, TransferWheelSubsystem transferWheelSubsystem) {
+  public ShootThenMoveAway(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, ShooterSubsystem shooterSubsystem, LimelightSubsystem limelightSubsystem, TurretSubsystem turretSubsystem, FeederSubsystem feederSubsystem) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, -30, PIDConstants.GyrokP30, PIDConstants.GyrokI30, PIDConstants.GyrokD30), new AlignAndShoot(turretSubsystem, limelightSubsystem, shooterSubsystem, transferWheelSubsystem),new StraightWithMotionMagic(driveBaseSubsystem, -80.88));
+    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, -30, PIDConstants.GyrokP30, PIDConstants.GyrokI30, PIDConstants.GyrokD30), new AlignAndShoot(turretSubsystem, limelightSubsystem, shooterSubsystem, feederSubsystem),new StraightWithMotionMagic(driveBaseSubsystem, -80.88));
   }
 }
