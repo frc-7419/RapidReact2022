ef77a1aec24595a18396bcd490b1529c3edb8489
Aarav Sharma <aaravlikestennis@gmail.com>
2022-03-03 13:12:23 -0800

    changed to ParallelCommandGroup and added a sequential() method so that straight and turn could run sequentially while intake and loader could run in parallel (all the time)

--- src/main/java/frc/robot/subsystems/autos/ShootGetSecondBallShootOneTurn.java
+++ src/main/java/frc/robot/subsystems/autos/ShootGetSecondBallShootOneTurn.java
@@ -2,6 +2,7 @@
 package frc.robot.subsystems.autos;
 
 import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.PIDConstants;
@@ -13,10 +14,12 @@ import frc.robot.subsystems.gyro.TurnWithGyroClosedLoop;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.RunIntake;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
+import frc.robot.subsystems.loader.LoaderSubsystem;
+import frc.robot.subsystems.loader.RunLoader;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.turret.TurretSubsystem;
 
-public class ShootGetSecondBallShootOneTurn extends SequentialCommandGroup {
+public class ShootGetSecondBallShootOneTurn extends ParallelCommandGroup {
 
     /* comments from Karan:
     you dont need to declare GyroSubsystem since its alr being injected
@@ -26,27 +29,16 @@ public class ShootGetSecondBallShootOneTurn extends SequentialCommandGroup {
 
     public ShootGetSecondBallShootOneTurn(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, 
     IntakeSubsystem intakeSubsystem, TurretSubsystem turretSubsystem, LimelightSubsystem limelightSubsystem, 
-    ShooterSubsystem shooterSubsystem, FeederSubsystem feederSubsystem) { //add parameters
-        //Robot is initially facing the hub. We then shoot the ball. Next we will turn the robot so that it can go back
-        //and collect the second ball and then shoot it
-        // addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 180, PIDConstants.GyrokP180, PIDConstants.GyrokI180, PIDConstants.GyrokD180)); //180 degree turn. 
-        // //Decorator where if the command doesn't finish in that time interval it will move on
-        // addCommands(new WaitCommand(0.25));
-        addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 67)); //The robot will ideally be positioned toward
-        //the middle of the tarmac so it will have to move sttraight about half of the distance between the hub and the ball
-        //to reach the ball
-        addCommands(new WaitCommand(0.25));
-        addCommands(new RunIntake(intakeSubsystem, 0.5));
-
-        //Here, we will collect the ball and then turn around and then shoot it 
-        addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 180, PIDConstants.GyrokP180, PIDConstants.GyrokI180, PIDConstants.GyrokD180)); //180 degree turn
-        addCommands(new WaitCommand(0.25));
-        addCommands(new StraightWithMotionMagic(driveBaseSubsystem, 67)); //The robot will ideally be positioned toward
-        //here after moving 58.08 inches, it will return back to its orignal position and then shoot *twice*
-        addCommands(new AlignAndShoot(turretSubsystem, limelightSubsystem, shooterSubsystem, feederSubsystem));
-
-        //changed the kD to 0.0001685
-
-        
+    ShooterSubsystem shooterSubsystem, FeederSubsystem feederSubsystem, LoaderSubsystem loaderSubsystem) { 
+        addCommands(
+            sequence(new StraightWithMotionMagic(driveBaseSubsystem, 67), 
+            new WaitCommand(0.25),
+            new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 180, PIDConstants.GyrokP180, PIDConstants.GyrokI180, PIDConstants.GyrokD180),
+            new WaitCommand(0.25), 
+            new AlignAndShoot(turretSubsystem, limelightSubsystem, shooterSubsystem, feederSubsystem)
+            )
+        );
+        addCommands(new RunIntake(intakeSubsystem, 1));
+        addCommands(new RunLoader(loaderSubsystem, 1));
     }
 }
