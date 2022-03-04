d914dd303e4b0f7d1ee1d46c98c9010f4e666a95
AaravWattal <61303690+AaravWattal@users.noreply.github.com>
2022-03-04 14:27:01 -0800

    arms subsystem

---
+++ src/main/java/frc/robot/subsystems/arms/ArmsSubsystem.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.arms;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmsSubsystem extends SubsystemBase {
+  //one motor controls both arms
+  private TalonSRX armTalon;
+  /** Creates a new ArmsSubsystem. */
+  public ArmsSubsystem() {
+    this.armTalon = new TalonSRX(11); //temporary ID
+    brake();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public void setPower(double power) {
+    armTalon.set(ControlMode.PercentOutput, power);
+  }
+  public void brake() {
+    armTalon.setNeutralMode(NeutralMode.Brake);
+  }
+  public void coast() {
+    armTalon.setNeutralMode(NeutralMode.Coast);
+  }
+}
