bac7f03f3ea67d39d3a80a43eca523195033241e
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-03-05 11:51:42 -0800

    Some initial changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,15 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.autos.Period1FourBallAuton;
 import frc.robot.subsystems.autos.Period1ThreeBallClose;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
+import frc.robot.subsystems.feeder.FeederSubsystem;
 import frc.robot.subsystems.gyro.GyroSubsystem;
+import frc.robot.subsystems.intake.IntakeSolenoidSubsystem;
+import frc.robot.subsystems.limelight.LimelightSubsystem;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
+import frc.robot.subsystems.turret.TurretSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -20,15 +26,17 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-
   // private final XboxController joystick = new XboxController(0);
   private final DriveBaseSubsystem driveBaseSubsystem = new DriveBaseSubsystem();
   private final GyroSubsystem gyroSubsystem = new GyroSubsystem();
-  //private final Period1AutonPath period1AutonPath = new Period1AutonPath(driveBaseSubsystem, gyroSubsystem);
-  private final Period1ThreeBallClose period1AutonPath = new Period1ThreeBallClose(driveBaseSubsystem, gyroSubsystem);
+  private final TurretSubsystem turretSubsystem = new TurretSubsystem();
+  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+  private final LimelightSubsystem limelightSubsystem  = new LimelightSubsystem();
+  private final IntakeSolenoidSubsystem intakeSolenoidSubsystem  = new IntakeSolenoidSubsystem();
+  private final FeederSubsystem feederSubsystem = new FeederSubsystem();
 
   // private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-  
+  private final Period1FourBallAuton period1FourBallAuton = new Period1FourBallAuton(driveBaseSubsystem, gyroSubsystem, turretSubsystem, shooterSubsystem, limelightSubsystem, intakeSolenoidSubsystem, feederSubsystem);
 
   public RobotContainer() {
     configureButtonBindings();
@@ -40,7 +48,7 @@ public class RobotContainer {
   private void smartDashboardBindings() {}
 
   public Command getAutonomousCommand() {
-    return period1AutonPath;
+    return period1FourBallAuton;
   }
 
   // set default commands here
