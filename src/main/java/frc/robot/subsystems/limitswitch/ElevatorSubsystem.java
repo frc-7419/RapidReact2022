e92e76469b78a296680379a5051dd775ab784740
braden0m <95321149+braden0m@users.noreply.github.com>
2022-03-07 10:32:25 -0800

    TalonFX version of elevator with limit switch

---
+++ src/main/java/frc/robot/subsystems/limitswitch/ElevatorSubsystem.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.limitswitch;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class ElevatorSubsystem extends SubsystemBase {
+  private TalonFX elevatorLeft;
+  private TalonFX elevatorRight;
+  /** Creates a new ElevatorSubsystem. */
+  public ElevatorSubsystem() {
+    elevatorLeft = new TalonFX(50);
+    elevatorRight = new TalonFX(51);
+    elevatorRight.setInverted(true);
+    }
+  
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  public void setPower(double power) {
+    elevatorLeft.set(ControlMode.PercentOutput, power);
+    elevatorRight.set(ControlMode.PercentOutput, power);
+  }
+
+
+  public void brake() {
+    elevatorLeft.setNeutralMode(NeutralMode.Brake);
+    elevatorRight.setNeutralMode(NeutralMode.Brake);
+}
+}
---
+++ src/main/java/frc/robot/subsystems/limitswitch/RunElevatorWithLimitSwitch.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.limitswitch;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
+public class RunElevatorWithLimitSwitch extends CommandBase {
+  /** Creates a new RunElevatorWithLimitSwitch. */
+  private ElevatorSubsystem elevatorSubsystem;
+  private XboxController joystick;
+
+  private DigitalInput topLimitSwitch;
+  private DigitalInput bottomLimitSwitch;
+
+  public RunElevatorWithLimitSwitch(ElevatorSubsystem elevatorSubsystem, XboxController joystick) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.elevatorSubsystem = elevatorSubsystem;
+    this.joystick = joystick;
+    topLimitSwitch = new DigitalInput(1);
+    bottomLimitSwitch = new DigitalInput(0);
+    addRequirements(elevatorSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+    SmartDashboard.putBoolean("Top Limit Switch", topLimitSwitch.get());
+    SmartDashboard.putBoolean("Bottom Limit Switch", bottomLimitSwitch.get());
+
+    SmartDashboard.putNumber("joystick out", joystick.getRightY());
+
+    if (!topLimitSwitch.get() && joystick.getRightY() < 0) {
+      elevatorSubsystem.setPower(0);
+      // elevatorSubsystem.brake();
+    } else if (!bottomLimitSwitch.get() && joystick.getRightY() > 0) {
+      elevatorSubsystem.setPower(0);
+      // elevatorSubsystem.brake();
+    } else if (joystick.getRightY() != 0) {
+      elevatorSubsystem.setPower(0.3 * joystick.getRightY());
+    } else {
+      elevatorSubsystem.setPower(0);
+      elevatorSubsystem.brake();
+    }
+      
+    }
+    // elevatorSubsystem.setPower(0.3 * joystick.getRightY());
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
