75d4f7cd946a1a0b712c20d31d3b42a57791cbcb
AaravWattal <61303690+AaravWattal@users.noreply.github.com>
2022-03-10 10:26:05 -0800

    sparkmax changes from talon

--- src/main/java/frc/robot/subsystems/arms/ArmsSubsystem.java
+++ src/main/java/frc/robot/subsystems/arms/ArmsSubsystem.java
@@ -7,16 +7,20 @@ package frc.robot.subsystems.arms;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CanIds;
 
 public class ArmsSubsystem extends SubsystemBase {
   //one motor controls both arms
-  private TalonSRX armTalon;
+  private CANSparkMax armCAN;
   /** Creates a new ArmsSubsystem. */
   public ArmsSubsystem() {
-    this.armTalon = new TalonSRX(CanIds.armsTalon.id); //temporary ID
+    this.armCAN = new CANSparkMax(CanIds.armCAN.id, MotorType.kBrushless); //temporary ID
     // brake();
   }
 
@@ -25,12 +29,12 @@ public class ArmsSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
   }
   public void setPower(double power) {
-    armTalon.set(ControlMode.PercentOutput, power);
+    armCAN.set(power);
   }
   public void brake() {
-    armTalon.setNeutralMode(NeutralMode.Brake);
+    armCAN.setIdleMode(IdleMode.kBrake);
   }
   public void coast() {
-    armTalon.setNeutralMode(NeutralMode.Coast);
+    armCAN.setIdleMode(IdleMode.kCoast);
   }
 }
