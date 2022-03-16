454c82b1d9aad69e6151e90c7b1cac6a21bca243
Keshav Badrinath <keshavbadrinath06@gmail.com>
2022-03-16 12:25:13 -0700

    changed spark to talon

--- src/main/java/frc/robot/subsystems/encoders/RunTurretWithJoystick.java
+++ src/main/java/frc/robot/subsystems/encoders/RunTurretWithJoystick.java
@@ -5,20 +5,19 @@
 package frc.robot.subsystems.encoders;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class RunSparkMaxWithJoystick extends CommandBase {
+public class RunTurretWithJoystick extends CommandBase {
   /** Creates a new RunSparkMaxWithJoystick. */
-  private SparkMaxSubsystem sparkMaxSubsystem;
+  private TurretSubsystem turretSubsystem;
   private XboxController joystick;
 
 
-  public RunSparkMaxWithJoystick(SparkMaxSubsystem sparkMaxSubsystem, XboxController joystick) {
-    this.sparkMaxSubsystem = sparkMaxSubsystem;
+  public RunTurretWithJoystick(TurretSubsystem turretSubsystem, XboxController joystick) {
+    this.turretSubsystem = turretSubsystem;
     this.joystick = joystick;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(sparkMaxSubsystem);
+    addRequirements(turretSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -28,13 +27,13 @@ public class RunSparkMaxWithJoystick extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    sparkMaxSubsystem.setPower(joystick.getLeftY()*0.15);
+    turretSubsystem.setPower(joystick.getLeftY()*0.15);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    sparkMaxSubsystem.setPower(0);
+    turretSubsystem.setPower(0);
   }
 
   // Returns true when the command should end.
---
+++ src/main/java/frc/robot/subsystems/encoders/TurretSubsystem.java
@@ -0,0 +1,73 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.encoders;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TurretSubsystem extends SubsystemBase {
+  /** Creates a new  */
+  private TalonFX talonFX;
+  private DigitalInput forwardLimitSwitch;
+  private DigitalInput reverseLimitSwitch;
+
+  private boolean forwardLimitDetected = false;
+  private boolean reverseLimitDetected = false;
+
+  public TurretSubsystem() {
+    //change later
+    talonFX = new TalonFX(69);
+    forwardLimitSwitch = new DigitalInput(0);
+    reverseLimitSwitch = new DigitalInput(1);
+    // canSparkMax.burnFlash();
+
+  }
+  public void setPower(double power) {
+    coast();
+    talonFX.set(ControlMode.PercentOutput, power);
+  }
+
+  public void brake() {
+    talonFX.setNeutralMode(NeutralMode.Brake);
+  }
+  public void coast() {
+    talonFX.setNeutralMode(NeutralMode.Coast);
+  }
+
+  public DigitalInput getForwardLimitSwitch() {
+    return forwardLimitSwitch;
+  } 
+  public DigitalInput getReverseLimitSwitch() {
+    return reverseLimitSwitch;
+  }
+
+
+  @Override
+  public void periodic() {
+    if (getReverseLimitSwitch().get() && !reverseLimitDetected) {
+      reverseLimitDetected = true;
+      talonFX.configReverseSoftLimitThreshold(talonFX.getSelectedSensorPosition(), 0);
+      talonFX.configReverseSoftLimitEnable(true, 0);
+    } 
+    if (getForwardLimitSwitch().get() && !forwardLimitDetected) {
+      forwardLimitDetected = true;
+      talonFX.configForwardSoftLimitThreshold(talonFX.getSelectedSensorPosition(), 0);
+      talonFX.configForwardSoftLimitEnable(true, 0);
+    } 
+    SmartDashboard.putBoolean("Reverse Limit Detected", reverseLimitDetected);
+
+    SmartDashboard.putBoolean("Forward Limit Detected", forwardLimitDetected);
+
+    SmartDashboard.putBoolean("Forward Limit Switch", forwardLimitSwitch.get());
+    SmartDashboard.putBoolean("Reverse Limit Switch", reverseLimitSwitch.get());
+    SmartDashboard.putNumber("Turret Encoder Position", talonFX.getSelectedSensorPosition());
+    
+  }
+}
