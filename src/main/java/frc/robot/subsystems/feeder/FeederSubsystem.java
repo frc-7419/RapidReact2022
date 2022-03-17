616bb24f752e60fa787b9d8bec354d6b8f2eaaec
Karan Saini <karansaini117@gmail.com>
2022-03-17 00:13:49 -0700

    NC

--- src/main/java/frc/robot/subsystems/feeder/FeederSubsystem.java
+++ src/main/java/frc/robot/subsystems/feeder/FeederSubsystem.java
@@ -6,20 +6,19 @@ package frc.robot.subsystems.feeder;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CanIds;
 
 public class FeederSubsystem extends SubsystemBase {
-  private TalonSRX transferWheel;
+  private TalonSRX feeder;
 
   public FeederSubsystem() {
-    transferWheel = new TalonSRX(CanIds.feederVictor.id);
+    feeder = new TalonSRX(CanIds.feederVictor.id);
   }
 
   public void setPower(double power) {
-    transferWheel.set(ControlMode.PercentOutput, power);
+    feeder.set(ControlMode.PercentOutput, power);
   }
 
   @Override
