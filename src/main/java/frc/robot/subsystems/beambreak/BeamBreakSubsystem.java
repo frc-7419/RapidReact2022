e7f080e5e1e2c2c6fa4a4ed1f6e8c8e571259543
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-03-19 15:45:16 -0700

    Updated build.gradle for 2022.4.1 and modified the beam break subsystem's cargo detection using currentTimeMillis instead of waitCommand

--- src/main/java/frc/robot/subsystems/beambreak/BeamBreakSubsystem.java
+++ src/main/java/frc/robot/subsystems/beambreak/BeamBreakSubsystem.java
@@ -5,22 +5,50 @@
 package frc.robot.subsystems.beambreak;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class BeamBreakSubsystem extends SubsystemBase {
   private DigitalInput beamBreakReceiver;
   private int detections = 0;
+  private Long startTime;
   /** Creates a new BeamBreakSubsystem. */
   public BeamBreakSubsystem() { 
     beamBreakReceiver = new DigitalInput(5);
-
+    startTime = System.currentTimeMillis();
   }
 
   @Override
   public void periodic() {
     SmartDashboard.putBoolean("beamBreak: ", beamBreakReceiver.get());
-    // This method will be called once per scheduler run
+    // WaitCommand(10);
+    // // This method will be called once per scheduler run
+
+    // if (detections < 2) {
+    //   if (beamBreakReceiver.get() == true) {
+    //     detections++;
+    //   }
+    // } else {
+    //   WaitCommand(10);
+    //   detections -= 2;
+    // } 
+    long endTime = System.currentTimeMillis();
+
+    if (detections >= 0 && detections <= 2) {
+      if (getBeamBreakActivated()) {
+        detections++;
+      }
+      else {
+        detections--;
+      }
+
+      if (endTime-startTime == 10000){
+        startTime = endTime;
+        detections = 0;
+      }
+    }
   }
 
   public boolean getBeamBreakActivated() {
@@ -30,13 +58,4 @@ public class BeamBreakSubsystem extends SubsystemBase {
   public DigitalInput getBeamBreakReceiver() {
     return beamBreakReceiver;
   }
-
-  if (detections < 2) {
-    if (beamBreakReceiver.get() == true) {
-      detections++;
-    }
-  } else {
-    new WaitCommand(10);
-    detections -= 2;
-  } 
 }
