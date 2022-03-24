c99f0f81ccfba3da268a4eb273ef12ab7121eccb
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-03-24 08:30:03 -0700

    Auton for today's practice tournament

--- src/main/java/frc/robot/subsystems/autos/ShootThenMoveAway.java
+++ src/main/java/frc/robot/subsystems/autos/ShootThenMoveAway.java
@@ -13,8 +13,12 @@ import frc.robot.subsystems.feeder.FeederSubsystem;
 import frc.robot.subsystems.gyro.GyroSubsystem;
 import frc.robot.subsystems.gyro.TurnWithGyroClosedLoop;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
+import frc.robot.subsystems.loader.LoaderSubsystem;
+import frc.robot.subsystems.loader.RunLoader;
+import frc.robot.subsystems.shooter.GetToTargetVelocity;
 import frc.robot.subsystems.shooter.GetToTargetVelocityWithLimelight;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
+import frc.robot.subsystems.turret.AlignTurret;
 import frc.robot.subsystems.turret.TurretSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -22,9 +26,16 @@ import frc.robot.subsystems.turret.TurretSubsystem;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ShootThenMoveAway extends SequentialCommandGroup {
   /** Creates a new ShootThenMoveAway. */
-  public ShootThenMoveAway(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, ShooterSubsystem shooterSubsystem, LimelightSubsystem limelightSubsystem, TurretSubsystem turretSubsystem, FeederSubsystem feederSubsystem, BeamBreakSubsystem beamBreakSubsystem) {
+  public ShootThenMoveAway(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, ShooterSubsystem shooterSubsystem, LimelightSubsystem limelightSubsystem, TurretSubsystem turretSubsystem, FeederSubsystem feederSubsystem, BeamBreakSubsystem beamBreakSubsystem, LoaderSubsystem loaderSubsystem) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, -30, PIDConstants.GyrokP30, PIDConstants.GyrokI30, PIDConstants.GyrokD30), new AlignAndShoot(turretSubsystem, limelightSubsystem, shooterSubsystem, feederSubsystem, beamBreakSubsystem, 1),new StraightWithMotionMagic(driveBaseSubsystem, -80.88));
+    //addCommands(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, -30, PIDConstants.GyrokP30, PIDConstants.GyrokI30, PIDConstants.GyrokD30), new AlignAndShoot(turretSubsystem, limelightSubsystem, shooterSubsystem, feederSubsystem, beamBreakSubsystem, 1),new StraightWithMotionMagic(driveBaseSubsystem, -80.88));
+    addCommands(
+      parallel(new RunLoader(loaderSubsystem, 1),new GetToTargetVelocity(shooterSubsystem, 9850, 6150),
+      sequence(
+        new StraightWithMotionMagic(driveBaseSubsystem, -80.88)
+      )
+      )
+    );
   }
 }
