e4da12b89887b89029bb9841f229954445a2f867
AaravWattal <61303690+AaravWattal@users.noreply.github.com>
2022-03-29 13:41:04 -0700

    auton command list

---
+++ src/main/java/frc/robot/subsystems/autos/SvrThreeBallAuto.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.autos;
+
+public class SvrThreeBallAuto {
+    
+}
+
+
+// shoot preload
+// turn 180
+// forward to ball while intake (parallel)
+// turn toward third ball
+// forward to third ball while intake (parallel)
+// turn toward hub
+// shoot both
