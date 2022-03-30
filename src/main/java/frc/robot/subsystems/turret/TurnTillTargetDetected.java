637ee945ef6edf818b3c250bb3c806291444bc9a
Karan Saini <karansaini117@gmail.com>
2022-03-30 00:07:51 -0700

    added TurnTillTargetDetected cmd

--- src/main/java/frc/robot/subsystems/turret/TurnTillTargetDetected.java
+++ src/main/java/frc/robot/subsystems/turret/TurnTillTargetDetected.java
@@ -33,7 +33,9 @@ public class TurnTillTargetDetected extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    turretSubsystem.setPower(0);
+  }
 
   // Returns true when the command should end.
   @Override
