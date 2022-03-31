66368524eca7626584a7b11ed947106142209207
SnehaG100 <email2sneha.g@gmail.com>
2022-03-31 16:26:58 -0700

    Added the rainbow method

--- src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
@@ -49,4 +49,19 @@ public class LEDSubsystem extends SubsystemBase {
     led.setData(ledBuffer);
     led.stop();
   }
+
+  public void rainbowLED(int rainbowFirstPixelHue) {
+    // For every pixel
+    for (var i = 0; i < ledBuffer.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      final var hue = (rainbowFirstPixelHue + (i * 180 / ledBuffer.getLength())) % 180;
+      // Set the value
+      ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    rainbowFirstPixelHue += 3;
+    // Check bounds
+    rainbowFirstPixelHue %= 180;
+  }
 }
