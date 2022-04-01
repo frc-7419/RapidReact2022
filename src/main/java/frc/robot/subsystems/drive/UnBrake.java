9484fad0f4631c17d26b3f68242632b47fedb117
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-03-31 21:03:24 -0700

    Quick change in UnBrake.java

--- src/main/java/frc/robot/subsystems/drive/UnBrake.java
+++ src/main/java/frc/robot/subsystems/drive/UnBrake.java
@@ -18,7 +18,6 @@ public class UnBrake extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    driveBaseSubsystem.coast();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
