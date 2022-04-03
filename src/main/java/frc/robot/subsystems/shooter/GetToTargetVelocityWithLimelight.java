ad34e0e5a38f34b2f24d9479212a9986bafe165a
Karan Saini <karansaini117@gmail.com>
2022-04-03 10:36:01 -0700

    new gttvwl

--- src/main/java/frc/robot/subsystems/shooter/GetToTargetVelocityWithLimelight.java
+++ src/main/java/frc/robot/subsystems/shooter/GetToTargetVelocityWithLimelight.java
@@ -1,12 +1,11 @@
 package frc.robot.subsystems.shooter;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.team7419.math.UnitConversions;
+import com.team7419.InterpolatedTreeMap;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.LimelightConstants;
-import frc.robot.Constants.RobotConstants;
+import frc.robot.Constants;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
 
 public class GetToTargetVelocityWithLimelight extends CommandBase {
@@ -14,48 +13,54 @@ public class GetToTargetVelocityWithLimelight extends CommandBase {
   private ShooterSubsystem shooterSubsystem;
   private LimelightSubsystem limelightSubsystem;
 
-  private double kP;
-  private double kI;
-  private double tkF;
-  private double bkF;
+  private InterpolatedTreeMap topShooterReferencePoints;
+  private InterpolatedTreeMap bottomShooterReferencePoints;
 
-  private double initialVelocity;
+  private double bKp = 0;
+  private double tKp = 0;
 
-  // private double targetRPM;
-  private double topTargetRPM;
-  private double bottomTargetRPM;
+  private double topTargetVelocity;
+  private double bottomTargetVelocity;
 
-  
   public GetToTargetVelocityWithLimelight(ShooterSubsystem shooterSubsystem, LimelightSubsystem limelightSubsystem) {
     this.shooterSubsystem = shooterSubsystem;
     this.limelightSubsystem = limelightSubsystem;
     addRequirements(shooterSubsystem);
+
+    topShooterReferencePoints = new InterpolatedTreeMap();
+    bottomShooterReferencePoints = new InterpolatedTreeMap();
+
+    // config reference points from constants file
+    shooterSubsystem.configInterpolatedTreeMapReferencePoints(Constants.kDistanceToTopShooterVelocity, topShooterReferencePoints);
+    shooterSubsystem.configInterpolatedTreeMapReferencePoints(Constants.kDistanceToBottomShooterVelocity, bottomShooterReferencePoints);
   }
 
   @Override
   public void initialize() {
-    // SmartDashboard.putBoolean("Shooter Running", false);
+    topTargetVelocity = topShooterReferencePoints.get(limelightSubsystem.getDistance()).doubleValue();
+    bottomTargetVelocity = bottomShooterReferencePoints.get(limelightSubsystem.getDistance()).doubleValue();
 
-    // redo math?
-    
-    topTargetRPM = UnitConversions.mpsToRPM(initialVelocity, RobotConstants.topShooterWheelRadius);
-    bottomTargetRPM = UnitConversions.mpsToRPM(initialVelocity, RobotConstants.bottomShooterWheelRadius);
+    shooterSubsystem.setTopPIDF(tKp, 0, 0, 0);
+    shooterSubsystem.setBottomPIDF(bKp, 0, 0, 0);
 
-    shooterSubsystem.setTopPIDF(kP, kI, 0, shooterSubsystem.computeTopkF(topTargetRPM));
-    shooterSubsystem.setBottomPIDF(kP, kI, 0, shooterSubsystem.computeBottomkF(bottomTargetRPM));
-    
-    shooterSubsystem.setTopTargetRawVelocity(UnitConversions.rpmToRawSensorVelocity(topTargetRPM, 2048));
-    shooterSubsystem.setBottomTargetRawVelocity(UnitConversions.rpmToRawSensorVelocity(bottomTargetRPM, 2048));
+    shooterSubsystem.setTopClosedLoopVelocity(topTargetVelocity);
+    shooterSubsystem.setBottomClosedLoopVelocity(bottomTargetVelocity);
+
     
   }
 
   @Override
   public void execute() {
-    // SmartDashboard.putBoolean("Shooter Running", true);
+    topTargetVelocity = topShooterReferencePoints.get(limelightSubsystem.getDistance()).doubleValue();
+    bottomTargetVelocity = bottomShooterReferencePoints.get(limelightSubsystem.getDistance()).doubleValue();
 
-    shooterSubsystem.getTopTalon().set(ControlMode.Velocity, UnitConversions.rpmToRawSensorVelocity(topTargetRPM, 2048));
-    shooterSubsystem.getBottomTalon().set(ControlMode.Velocity, UnitConversions.rpmToRawSensorVelocity(bottomTargetRPM, 2048));
+    shooterSubsystem.setTopPIDF(tKp, 0, 0 , 0);
+    shooterSubsystem.setBottomPIDF(bKp, 0, 0, 0);
 
+    shooterSubsystem.setTopClosedLoopVelocity(topTargetVelocity);
+    shooterSubsystem.setBottomClosedLoopVelocity(bottomTargetVelocity);
+
+  
     // SmartDashboard.putBoolean("Top On Target", shooterSubsystem.topOnTarget());
     // SmartDashboard.putBoolean("Bottom on Target", shooterSubsystem.bottomOnTarget());
     // SmartDashboard.putBoolean("Both on Target", shooterSubsystem.bothOnTarget());
@@ -63,8 +68,8 @@ public class GetToTargetVelocityWithLimelight extends CommandBase {
 
   @Override
   public void end(boolean interrupted) {
+    SmartDashboard.putBoolean("Shooter Running", false);
     shooterSubsystem.off();
-    // SmartDashboard.putBoolean("Shooter Running", false);
   }
 
   @Override
