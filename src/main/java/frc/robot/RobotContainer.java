8af2fe13ca9055dd4fa1eaf11da6249b73183af1
Karan Saini <karansaini117@gmail.com>
2022-04-02 17:05:03 -0700

    rem old

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.PIDConstants;
 import frc.robot.Constants.PowerConstants;
@@ -14,7 +15,6 @@ import frc.robot.commands.RunIntakeAndLoaderWithJoystick;
 import frc.robot.subsystems.arms.ArmsSubsystem;
 import frc.robot.subsystems.arms.CoastArms;
 import frc.robot.subsystems.arms.RunArmsWithJoystick;
-import frc.robot.subsystems.autos.OneBallAuto;
 import frc.robot.subsystems.drive.ArcadeDrive;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 import frc.robot.subsystems.drive.NewArcadeDrive;
@@ -70,8 +70,7 @@ public class RobotContainer {
   private final NewArcadeDrive newArcadeDrive = new NewArcadeDrive(joystick1, newDriveBaseSubsystem, 0.95, 0.75);
 
   // auto
-  private SendableChooser<Command> autonChooser = new SendableChooser<>();
-  private final OneBallAuto oneBallAuto = new OneBallAuto(driveBaseSubsystem, gyroSubsystem, shooterSubsystem, limelightSubsystem, feederSubsystem, loaderSubsystem);
+  // private SendableChooser<Command> autonChooser = new SendableChooser<>();
 
   public RobotContainer() {
     configureButtonBindings();
@@ -84,30 +83,6 @@ public class RobotContainer {
     new JoystickButton(joystick2, XboxController.Button.kLeftBumper.value)
     .whileHeld(new AlignTurretDefault(turretSubsystem, limelightSubsystem));
 
-    // edge of tarmac 
-      // new JoystickButton(joystick2, XboxController.Button.kX.value)
-      // .whileHeld(new GetToTargetVelocity(shooterSubsystem, 9850, 6150, 0.0485, 0.0495));
-
-      new JoystickButton(joystick2, XboxController.Button.kX.value)
-      .whileHeld(new GetToTargetVelocity(shooterSubsystem, 8530, 8160, 0.04876, 0.04772));
-    
-    // toggle to maintain elevator position
-    new DoubleButton(
-      new JoystickButton(joystick2, XboxController.Button.kA.value),
-      new JoystickButton(joystick2, XboxController.Button.kB.value))
-      .toggleWhenPressed(new MaintainElevatorPosition(elevatorSubsystem));
-
-      // lower hub shot, consistent
-    new JoystickButton(joystick2, XboxController.Button.kA.value)
-    .whileHeld(new GetToTargetVelocity(shooterSubsystem, 3500, 5450, 0.042, 0.0475));
-      
-    // long
-    // new JoystickButton(joystick2, XboxController.Button.kY.value)
-    // .whileHeld(new GetToTargetVelocity(shooterSubsystem, 11500, 6500, 0.04775, 0.0477425));
-
-    // new long
-    new JoystickButton(joystick2, XboxController.Button.kY.value)
-    .whileHeld(new GetToTargetVelocity(shooterSubsystem, 7900, 9900, 0.04874, 0.049));
 
 
     new JoystickButton(joystick2, XboxController.Button.kRightBumper.value)
@@ -127,15 +102,15 @@ public class RobotContainer {
   }
 
   private void configureAutoSelector() {
-    autonChooser.setDefaultOption("Preload Default", oneBallAuto);
-    // autonChooser.addOption("2 Ball", twoBallAuto);
-    // autonChooser.addOpton("3 Ball", threeBallAuto);
-    // autonChooser.addOption("5 Ball", fiveBallAuto);
-    SmartDashboard.putData(autonChooser);
+    // autonChooser.setDefaultOption("Preload Default", oneBallAuto);
+    // // autonChooser.addOption("2 Ball", twoBallAuto);
+    // // autonChooser.addOpton("3 Ball", threeBallAuto);
+    // // autonChooser.addOption("5 Ball", fiveBallAuto);
+    // SmartDashboard.putData(autonChooser);
   }
 
   public Command getAutonomousCommand() {
-    return autonChooser.getSelected();
+    return new WaitCommand(0);
   }
     
   public void setDefaultCommands() {
