b130eef3241176bb0d9af5ecf8ce7417c61810f9
Karan Saini <karansaini117@gmail.com>
2022-04-07 22:05:28 -0700

    added led stuff to one and two ball autos

--- src/main/java/frc/robot/subsystems/autos/OneBallAuto.java
+++ src/main/java/frc/robot/subsystems/autos/OneBallAuto.java
@@ -1,24 +1,30 @@
 package frc.robot.subsystems.autos;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 import frc.robot.subsystems.drive.StraightWithMotionMagic;
 import frc.robot.subsystems.feeder.FeederSubsystem;
 import frc.robot.subsystems.gyro.GyroSubsystem;
+import frc.robot.subsystems.led.LEDSubsystem;
+import frc.robot.subsystems.led.SetLEDColor;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
 import frc.robot.subsystems.loader.LoaderSubsystem;
 import frc.robot.subsystems.loader.RunLoader;
 import frc.robot.subsystems.shooter.GetToTargetVelocityArbitraryFeedforward;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
-public class OneBallAuto extends SequentialCommandGroup {
+public class OneBallAuto extends ParallelCommandGroup {
 
-    public OneBallAuto(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, ShooterSubsystem shooterSubsystem, LimelightSubsystem limelightSubsystem, FeederSubsystem feederSubsystem, LoaderSubsystem loaderSubsystem) {
+    public OneBallAuto(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, ShooterSubsystem shooterSubsystem, LimelightSubsystem limelightSubsystem, FeederSubsystem feederSubsystem, LoaderSubsystem loaderSubsystem, LEDSubsystem ledSubsystem) {
         addCommands(
-            new GetToTargetVelocityArbitraryFeedforward(shooterSubsystem, 9850, 6150, 0.0485, 0.0495).withTimeout(2),
-            parallel(new RunLoader(loaderSubsystem, 1.0), new GetToTargetVelocityArbitraryFeedforward(shooterSubsystem, 9850, 6150, 0.0485, 0.0495)).withTimeout(2.5),
-            new StraightWithMotionMagic(driveBaseSubsystem, -80.88)
+            sequence(
+                new GetToTargetVelocityArbitraryFeedforward(shooterSubsystem, 9850, 6150, 0.0485, 0.0495).withTimeout(2),
+                parallel(new RunLoader(loaderSubsystem, 1.0), new GetToTargetVelocityArbitraryFeedforward(shooterSubsystem, 9850, 6150, 0.0485, 0.0495)).withTimeout(2.5),
+                new StraightWithMotionMagic(driveBaseSubsystem, -80.88)
+            )
         );
+        addCommands(new SetLEDColor(ledSubsystem, limelightSubsystem));
     }
     
 }
--- src/main/java/frc/robot/subsystems/autos/TwoBallAuto.java
+++ src/main/java/frc/robot/subsystems/autos/TwoBallAuto.java
@@ -2,7 +2,7 @@
 package frc.robot.subsystems.autos;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.PIDConstants;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
@@ -13,6 +13,8 @@ import frc.robot.subsystems.gyro.GyroSubsystem;
 import frc.robot.subsystems.gyro.TurnWithGyroClosedLoop;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.RunIntake;
+import frc.robot.subsystems.led.LEDSubsystem;
+import frc.robot.subsystems.led.SetLEDColor;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
 import frc.robot.subsystems.loader.LoaderSubsystem;
 import frc.robot.subsystems.loader.RunLoader;
@@ -22,62 +24,65 @@ import frc.robot.subsystems.turret.AlignTurretDefault;
 import frc.robot.subsystems.turret.BrakeTurret;
 import frc.robot.subsystems.turret.TurretSubsystem;
 
-public class TwoBallAuto extends SequentialCommandGroup {
+public class TwoBallAuto extends ParallelCommandGroup {
 
-    public TwoBallAuto(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, ShooterSubsystem shooterSubsystem, FeederSubsystem feederSubsystem, LoaderSubsystem loaderSubsystem, IntakeSubsystem intakeSubsystem, TurretSubsystem turretSubsystem, LimelightSubsystem limelightSubsystem) {
+    public TwoBallAuto(DriveBaseSubsystem driveBaseSubsystem, GyroSubsystem gyroSubsystem, ShooterSubsystem shooterSubsystem, FeederSubsystem feederSubsystem, LoaderSubsystem loaderSubsystem, IntakeSubsystem intakeSubsystem, TurretSubsystem turretSubsystem, LimelightSubsystem limelightSubsystem, LEDSubsystem ledSubsystem) {
         addCommands(
-            parallel(new AlignTurretDefault(turretSubsystem, limelightSubsystem), new GetToTargetVelocity(shooterSubsystem, 37, 30))
-                .withInterrupt(() -> shooterSubsystem.bothOnTarget()).withTimeout(0.75), // gttv while aligning turret
+            sequence(
+                parallel(new AlignTurretDefault(turretSubsystem, limelightSubsystem), new GetToTargetVelocity(shooterSubsystem, 37, 30))
+                    .withInterrupt(() -> shooterSubsystem.bothOnTarget()).withTimeout(0.75), // gttv while aligning turret
 
-            // shoot preload
-            parallel(
-                new AlignTurretDefault(turretSubsystem, limelightSubsystem),
-                new GetToTargetVelocity(shooterSubsystem, 37, 30),
-                new RunFeeder(feederSubsystem, 1),
-                new RunLoader(loaderSubsystem, 1)
-            ).withTimeout(1.5), // tune time
-            
-            // new InstantCommand(intakeSolenoidSubsystem::retractSolenoid, intakeSolenoidSubsystem),
+                // shoot preload
+                parallel(
+                    new AlignTurretDefault(turretSubsystem, limelightSubsystem),
+                    new GetToTargetVelocity(shooterSubsystem, 37, 30),
+                    new RunFeeder(feederSubsystem, 1),
+                    new RunLoader(loaderSubsystem, 1)
+                ).withTimeout(1.5), // tune time
+                
+                // new InstantCommand(intakeSolenoidSubsystem::retractSolenoid, intakeSolenoidSubsystem),
 
-            // turn 180 while braking turret
-            new BrakeTurret(turretSubsystem)
-                .deadlineWith(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 175, 3, PIDConstants.GyrokP180, PIDConstants.GyrokI180, PIDConstants.GyrokD180))
-                .withTimeout(1.5),
+                // turn 180 while braking turret
+                new BrakeTurret(turretSubsystem)
+                    .deadlineWith(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 175, 3, PIDConstants.GyrokP180, PIDConstants.GyrokI180, PIDConstants.GyrokD180))
+                    .withTimeout(1.5),
 
-            new WaitCommand(0.25),  
-            
-            // deploy intake
-            // new InstantCommand(intakeSolenoidSubsystem::actuateSolenoid, intakeSolenoidSubsystem),
+                new WaitCommand(0.25),  
+                
+                // deploy intake
+                // new InstantCommand(intakeSolenoidSubsystem::actuateSolenoid, intakeSolenoidSubsystem),
 
-            // move forward and running intake + loader
-            parallel(
-                new RunIntake(intakeSubsystem, 1),
-                new RunLoader(loaderSubsystem, 0.6)
-            ).deadlineWith(new StraightWithMotionMagic(driveBaseSubsystem, 50))
-            .withTimeout(2.5),
-            
-            new WaitCommand(0.25),
+                // move forward and running intake + loader
+                parallel(
+                    new RunIntake(intakeSubsystem, 1),
+                    new RunLoader(loaderSubsystem, 0.6)
+                ).deadlineWith(new StraightWithMotionMagic(driveBaseSubsystem, 50))
+                .withTimeout(2.5),
+                
+                new WaitCommand(0.25),
 
-            // retract intake
-            // new InstantCommand(intakeSolenoidSubsystem::retractSolenoid, intakeSolenoidSubsystem),
+                // retract intake
+                // new InstantCommand(intakeSolenoidSubsystem::retractSolenoid, intakeSolenoidSubsystem),
 
-            // turn 180 while braking turret
-            new BrakeTurret(turretSubsystem)
-                .deadlineWith(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 182, 2, PIDConstants.GyrokP180, PIDConstants.GyrokI180, PIDConstants.GyrokD180)).withTimeout(2),
-            
-            parallel(new AlignTurretDefault(turretSubsystem, limelightSubsystem), new GetToTargetVelocity(shooterSubsystem, 43, 40))
-                .withInterrupt(() -> shooterSubsystem.bothOnTarget())
-                .withTimeout(1.15), // gttv while aligning turret
-            
-            // shoot second ball
-            parallel(
-                new AlignTurretDefault(turretSubsystem, limelightSubsystem),
-                new GetToTargetVelocity(shooterSubsystem, 45.5, 43), // mainting the specific velocity (to be tuned)
-                new RunLoader(loaderSubsystem, 1),
-                new RunFeeder(feederSubsystem, 0.5)
-            ).withTimeout(1.5),
+                // turn 180 while braking turret
+                new BrakeTurret(turretSubsystem)
+                    .deadlineWith(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 182, 2, PIDConstants.GyrokP180, PIDConstants.GyrokI180, PIDConstants.GyrokD180)).withTimeout(2),
+                
+                parallel(new AlignTurretDefault(turretSubsystem, limelightSubsystem), new GetToTargetVelocity(shooterSubsystem, 43, 40))
+                    .withInterrupt(() -> shooterSubsystem.bothOnTarget())
+                    .withTimeout(1.15), // gttv while aligning turret
+                
+                // shoot second ball
+                parallel(
+                    new AlignTurretDefault(turretSubsystem, limelightSubsystem),
+                    new GetToTargetVelocity(shooterSubsystem, 45.5, 43), // mainting the specific velocity (to be tuned)
+                    new RunLoader(loaderSubsystem, 1),
+                    new RunFeeder(feederSubsystem, 0.5)
+                ).withTimeout(1.5),
 
-            new InstantCommand(driveBaseSubsystem::coast, driveBaseSubsystem)
+                new InstantCommand(driveBaseSubsystem::coast, driveBaseSubsystem)
+            )
         );
+        addCommands(new SetLEDColor(ledSubsystem, limelightSubsystem));
     }
 }
