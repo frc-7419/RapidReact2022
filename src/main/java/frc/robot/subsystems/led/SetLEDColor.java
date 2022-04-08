023cba90c0170d2463747aba8a7dc49a6b59ad13
Karan Saini <karansaini117@gmail.com>
2022-04-07 22:10:00 -0700

    add set led color cmd

---
+++ src/main/java/frc/robot/subsystems/led/SetLEDColor.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.led;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.limelight.LimelightSubsystem;
+
+public class SetLEDColor extends CommandBase {
+  private LEDSubsystem ledSubsystem;
+  private LimelightSubsystem limelightSubsystem;
+
+  private int rainbowFirstPixelHue = 0;
+
+  public SetLEDColor(LEDSubsystem ledSubsystem, LimelightSubsystem limelightSubsystem) {
+    this.ledSubsystem = ledSubsystem;
+    this.limelightSubsystem = limelightSubsystem;
+    addRequirements(ledSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    ledSubsystem.startLed();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // ledSubsystem.rainbowLED(rainbowFirstPixelHue);
+    // ledSubsystem.startLed();
+    // rainbowFirstPixelHue += 3;
+    // rainbowFirstPixelHue %= 180;
+    if (limelightSubsystem.getTv() == 1.0 && Math.abs(limelightSubsystem.getTx()) < 1) {
+      ledSubsystem.setLEDColor(0, 255, 0);
+      // ledSubsystem.startLed();
+    }
+    else if (limelightSubsystem.getTv() == 1.0 && Math.abs(limelightSubsystem.getTx()) > 1) {
+      ledSubsystem.setLEDColor(0, 0, 255);
+      // ledSubsystem.startLed();
+    }
+    else {
+      ledSubsystem.setLEDColor(255, 0, 0);
+      // ledSubsystem.startLed();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    ledSubsystem.setLEDColor(255, 255, 0);
+    // ledSubsystem.stopLed();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
