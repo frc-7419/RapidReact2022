fc72ad7eae9bfa5f57aeaf8989c40d8113b2a1fd
Karan Saini <karansaini117@gmail.com>
2022-04-08 20:14:26 -0700

    added limelight veocities for second ball, since second ball has always missed so far

--- src/main/java/frc/robot/subsystems/autos/TwoBallAuto.java
+++ src/main/java/frc/robot/subsystems/autos/TwoBallAuto.java
@@ -20,6 +20,7 @@ import frc.robot.subsystems.limelight.LimelightSubsystem;
 import frc.robot.subsystems.loader.LoaderSubsystem;
 import frc.robot.subsystems.loader.RunLoader;
 import frc.robot.subsystems.shooter.GetToTargetVelocity;
+import frc.robot.subsystems.shooter.GetToTargetVelocityWithLimelight;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.turret.AlignTurretDefault;
 import frc.robot.subsystems.turret.BrakeTurret;
@@ -69,14 +70,29 @@ public class TwoBallAuto extends ParallelCommandGroup {
                 new BrakeTurret(turretSubsystem)
                     .deadlineWith(new TurnWithGyroClosedLoop(driveBaseSubsystem, gyroSubsystem, 182, 2, PIDConstants.GyrokP180, PIDConstants.GyrokI180, PIDConstants.GyrokD180)).withTimeout(2),
                 
-                parallel(new AlignTurretDefault(turretSubsystem, limelightSubsystem), new GetToTargetVelocity(shooterSubsystem, 43, 40))
+
+                // exact shooter velocities:
+                // parallel(new AlignTurretDefault(turretSubsystem, limelightSubsystem), new GetToTargetVelocity(shooterSubsystem, 43, 40))
+                //     .withInterrupt(() -> shooterSubsystem.bothOnTarget())
+                //     .withTimeout(1.15), // gttv while aligning turret
+
+                // gttv using limelight
+                parallel(new AlignTurretDefault(turretSubsystem, limelightSubsystem), new GetToTargetVelocityWithLimelight(shooterSubsystem, limelightSubsystem))
                     .withInterrupt(() -> shooterSubsystem.bothOnTarget())
                     .withTimeout(1.15), // gttv while aligning turret
                 
-                // shoot second ball
+                // // shoot second ball, exact velocities:
+                // parallel(
+                //     new AlignTurretDefault(turretSubsystem, limelightSubsystem),
+                //     new GetToTargetVelocity(shooterSubsystem, 45.5, 43), // mainting the specific velocity
+                //     new RunLoader(loaderSubsystem, 1),
+                //     new RunFeeder(feederSubsystem, 0.5)
+                // ).withTimeout(1.5),
+
+                // shoot second ball, using limelight velocity
                 parallel(
                     new AlignTurretDefault(turretSubsystem, limelightSubsystem),
-                    new GetToTargetVelocity(shooterSubsystem, 45.5, 43), // mainting the specific velocity (to be tuned)
+                    new GetToTargetVelocityWithLimelight(shooterSubsystem, limelightSubsystem), // mainting the specific velocity
                     new RunLoader(loaderSubsystem, 1),
                     new RunFeeder(feederSubsystem, 0.5)
                 ).withTimeout(1.5),
